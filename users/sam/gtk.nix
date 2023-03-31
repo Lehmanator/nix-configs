0906b7a29485d8f6a96e8344e7b9091520d97654
Sam Lehman <slehman@piwine.com>
2023-03-31 19:56:39 -0400

    users.profiles.gtk: enable fontconfig, specify theme names

--- users/sam/gtk.nix
+++ users/sam/gtk.nix
@@ -1,6 +1,16 @@
 { config, lib, pkgs, ... }: {
   imports = [];
 
+  # https://nix-community.github.io/home-manager/options.html#opt-gtk.enable
+
+  # Allow fontconfig to discover fonts & configs installed thru `home.packages` & `nix-env`
+  fonts.fontconfig.enable = true;
+
+  # --- GTK ---
+  gtk.enable = true;
+  gtk.iconTheme.name = "Adwaita";
+  gtk.iconTheme.package = pkgs.gnome.adwaita-icon-theme;
+  gtk.theme.name = "Adw-gtk3";
   gtk.theme.package = pkgs.adw-gtk3;
 
 }
