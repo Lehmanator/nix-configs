acda95406b39030b659ca81e8af46ada683e15b2
Sam Lehman <slehman@piwine.com>
2023-03-31 10:05:04 -0400

    profiles.shell-zsh: comments

--- profiles/shell-zsh.nix
+++ profiles/shell-zsh.nix
@@ -8,19 +8,31 @@
 }:
 {
   imports = [
+    # TODO: Put generic shell-agnostic settings here
     #./shell.nix
   ];
 
   programs.zsh = {
     enable = true;
+
+    # --- Completion ------------
     enableCompletion = true;
     enableBashCompletion = true;
     autosuggestions.enable = true;
+
+    # --- Syntax Highlighting ---
     syntaxHighlighting.enable = true;
     syntaxHighlighting.highlighters = [ "main" "brackets" "cursor" "line" ];
+
+    # --- Terminal --------------
     vteIntegration = true;
   };
+
+  # --- Prompt ----------------
+  # Prompt for Bash & ZSH
   programs.starship.enable = true;
 
+  # --- Default ---------------
+  # Make ZSH default for all users
   users.defaultUserShell = pkgs.zsh;
 }
