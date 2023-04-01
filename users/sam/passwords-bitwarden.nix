7ee09f08a44f63a79537bccbf178051552d873ef
Sam Lehman <slehman@piwine.com>
2023-03-31 20:33:17 -0400

    users.profiles: add passwords-bitwarden

---
+++ users/sam/passwords-bitwarden.nix
@@ -0,0 +1,17 @@
+{
+  config, lib, pkgs,
+  host,
+  ...
+}:
+{
+  imports = [
+  ];
+
+  programs.rbw.enable = true;
+  programs.rbw.settings = {
+    lock_timeout = 3600;
+    #email = "slehman@piwine.com";  # TODO: Use secret
+    #base_url = "bitwarden.";       # TODO: Use secret
+    #identity_url = "identity.";    # TODO: Use secret
+  };
+}
