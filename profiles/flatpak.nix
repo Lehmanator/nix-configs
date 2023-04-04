06444f51a37b54a73f09433ab3229ce5e08c0c65
Sam Lehman <slehman@piwine.com>
2023-04-04 08:25:00 -0400

    profiles.flatpak: add args, enable fontconfig, WIP empty activation script for rebuilding font cache.

--- profiles/flatpak.nix
+++ profiles/flatpak.nix
@@ -1,5 +1,21 @@
-{ self, system, userPrimary, inputs, config, lib, pkgs, ... }: {
+{ 
+  self, 
+  system, 
+  host, userPrimary, 
+  inputs, 
+  config, lib, pkgs,
+  ...
+}:
+{
+
   appstream.enable = true;
+
+  fonts.fontconfig.enable = true;
+  fonts.fontDir.enable = true;
+  system.activationScripts.flatpakSystem.text = ''
+  ''; # TODO: Replace with primary user
+    #fc-cache -rf
+    #ln -s /run/current-system/sw/share/X11/fonts /home/sam/.local/share/fonts
 
   services.flatpak.enable = true;
   services.packagekit.enable = true;
