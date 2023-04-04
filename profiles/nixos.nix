fb92a2156c6bba36fc8507cca46c1ace63fcf7ff
Sam Lehman <slehman@piwine.com>
2023-04-04 08:13:48 -0400

    profiles.nixos: fix activation, fix nixos-rebuild shell aliases, add new shell aliases

--- profiles/nixos.nix
+++ profiles/nixos.nix
@@ -7,20 +7,53 @@
   ...
 }:
 let
+  # TODO: Randomize & actually use
+  figs = [
+    "nancyj-underlined" "nvscript" "jazmine" "o8" "ogre"  "puffy" 
+    "rectangles"        "rev"      "roman"   "rowancap" 
+    "rozzo"             "cursive"  "script"  "slant" 
+    "standard"          "starwars" "thick"   "univers"  "whimsy"
+  ];
 in
 {
   # TODO: Abstract out sudo program
   environment.shellAliases = {
+    # --- systemd ----------------
     ctl = "systemctl";
     sctl = "sudo systemctl";
-
+    # --- Privileged Execution ---
     s = lib.mkIf config.security.sudo.enable "sudo";
     pk = "pkexec";
-
-    nbuild = "sudo nixos-rebuild switch";
-    nbuildvm = "sudo nixos-rebuild switch";
+    # --- Nix --------------------
+    n = "nix";
+    nb = "nix build";
+    build = "nix build";
+    flake = "nix flake";
+    profile = "nix profile";
+    repl = "nix repl";
+    run = "nix run";
+    store = "nix store";
+    # --- NixOS ------------------
+    nboot = "sudo nixos-rebuild boot";
+    nbuild = "sudo nixos-rebuild build";
+    nbuilddry = "sudo nixos-rebuild dry-build";
+    nbuildvm = "sudo nixos-rebuild build-vm";
+    nbuildvmb = "sudo nixos-rebuild build-vm-with-bootloader";
+    ndry = "sudo nixos-rebuild dry-activate";
     nswitch = "sudo nixos-rebuild switch";
     nswitchdry = "sudo nixos-rebuild switch";
     ntest = "sudo nixos-rebuild test";
+    nupdate = "sudo nixos-rebuild switch --upgrade";
+  };
+
+  # --- System Info --------------
+  environment.systemPackages = [ pkgs.figlet ];
+  system.activationScripts.hostInfo = {
+    # TODO: Change per system type
+    text = ''
+      ${pkgs.figlet}/bin/figlet -cf o8 "NixOS: ${config.networking.hostName}"
+      echo "successfully activated system: ${config.networking.hostName}"
+    '';
+    #deps = [ pkgs.figlet ];
   };
 }
