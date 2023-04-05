90f4ba4cd2995dfa484c450d6afb7c38403bdd50
Sam Lehman <slehman@piwine.com>
2023-04-05 00:03:43 -0400

    users.profiles.shell-zsh: terminal tab titles

--- users/sam/shell-zsh.nix
+++ users/sam/shell-zsh.nix
@@ -73,6 +73,20 @@ in
 
   # --- Initialization -------------------------------------
   programs.zsh.initExtra = ''
+    autoload -Uz add-zsh-hook
+    function xterm_title_precmd() {
+      print -Pn -- '\e]2;%n@%m %~\a'
+      [[ "$TERM" == 'screen'* ]] && print -Pn -- '\e_\005{g}%n\005{-}@\005{m}%m\005{-} \005{B}%~\005{-}\e\\'
+    }
+    function xterm_title_preexec () {
+      print -Pn -- '\e]2;%n@%m %~ %# ' && print -n -- "${"{(q)1}"}\a"
+      [[ "$TERM" == 'screen'* ]] && { print -Pn -- '\e_\005{g}%n\005{-}@\005{m}%m\005{-} \005{B}%~\005{-} %# ' && print -n -- "${"{(q)1}"}\e\\"; }
+    }
+
+    if [[ "$TERM" == (Eterm*|alacritty*|aterm*|foot*|gnome*|konsole*|kterm*|putty*|rxvt*|screen*|wezterm*|tmux*|xterm*) ]]; then
+      add-zsh-hook -Uz precmd  xterm_title_precmd
+      add-zsh-hook -Uz preexec xterm_title_preexec
+    fi
   '';
 
   # --- Plugins --------------------------------------------
