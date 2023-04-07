2db56e8d84782b2e8274a58708886dc62b0756dd
Sam Lehman <slehman@piwine.com>
2023-04-07 14:54:38 -0400

    users.profiles.shell: enable tealdeer

--- users/sam/shell.nix
+++ users/sam/shell.nix
@@ -44,21 +44,33 @@
   # --- Prompt ---------------
   programs.starship.enable = true;
 
-  home.packages = [
-    pkgs.figlet               # Print ASCII art text
-    pkgs.nix-zsh-completions  # Completions for Nix, NixOS, NixOps, & ecosystem
-    pkgs.with-shell           # Interactive shell where each command starts with cmd prefix
-    pkgs.zsh-nix-shell        # ZSH plugin that lets you use ZSH in nix-shell
-  ];
-
+  # --- Dev Shells -----------
   services.lorri.enable = true;
 
+  # --- Documentation --------
   manual = {
     html.enable = true;
     json.enable = true;
     manpages.enable = true;
   };
   news.display = "notify";
+  programs.tealdeer = {
+    enable = true;
+    settings = {
+      display.compact = true; 
+      display.use_pager = false;
+      updates.auto_update = true;
+      #style.example_variable.foreground = "cyan";
+    };
+  };
+
+  # --- Packages -------------
+  home.packages = [
+    pkgs.figlet               # Print ASCII art text
+    pkgs.nix-zsh-completions  # Completions for Nix, NixOS, NixOps, & ecosystem
+    pkgs.with-shell           # Interactive shell where each command starts with cmd prefix
+    pkgs.zsh-nix-shell        # ZSH plugin that lets you use ZSH in nix-shell
+  ];
 
   programs.fzf = {
     enable = true;
