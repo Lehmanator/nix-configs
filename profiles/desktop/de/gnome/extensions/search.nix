18c3f57a2887e4e934e57ffeb97f09d6ed37ac01
Sam Lehman <slehman@piwine.com>
2023-04-08 12:42:10 -0400

    profiles.gnome.extensions: add search extensions

--- profiles/desktop/de/gnome/extensions/search.nix
+++ profiles/desktop/de/gnome/extensions/search.nix
@@ -8,8 +8,13 @@
   ];
 
   environment.systemPackages = with pkgs.gnomeExtensions; [
+    duckduckgo-search-provider
+    google-search-provider
     fuzzy-app-search
+    keyman
     just-another-search-bar
+    remmina-search-provider
+    ssh-search-provider-reborn
   ];
 
 }
