8844a48e222648defa8b1701d5165ec6f5d54a1b
Sam Lehman <slehman@piwine.com>
2023-04-08 12:38:52 -0400

    profiles.nix: add more substituters

--- profiles/nix.nix
+++ profiles/nix.nix
@@ -79,6 +79,8 @@
   nix.settings.substituters = [
     "https://cache.nixos.org/"
     "https://nix-community.cachix.org/"
+    "https://nix-on-droid.cachix.org/"
+    "https://robotnix.cachix.org/"
     "https://nrdxp.cachix.org/"
     "https://numtide.cachix.org/"
     "https://snowflakeos.cachix.org/"
@@ -86,6 +88,7 @@
   nix.settings.trusted-substituters = [
     "https://hydra.nixos.org/"
     "https://nix-community.cachix.org/"
+    "https://nix-on-droid.cachix.org/"
     "https://nrdxp.cachix.org/"
     "https://numtide.cachix.org/"
     "https://snowflakeos.cachix.org/"
@@ -93,6 +96,7 @@
   nix.settings.trusted-public-keys = [
     "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "nix-on-droid.cachix.org-1:56snoMJTXmDRC1Ei24CmKoUqvHJ9XCp+nidK7qkMQrU="
     "nrdxp.cachix.org-1:Fc5PSqY2Jm1TrWfm88l6cvGWwz3s93c6IOifQWnhNW4="
     "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
   ];
