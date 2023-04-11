8256110eb809b12958b116f0ef0a04fcd7fb9aca
Sam Lehman <slehman@piwine.com>
2023-04-11 13:13:58 -0400

    users.profiles.gtk: add package: adw-gtk3, remove as gtk.theme.package because it doesnt toggle with light/dark mode

--- users/sam/gtk.nix
+++ users/sam/gtk.nix
@@ -10,7 +10,12 @@
   gtk.enable = true;
   gtk.iconTheme.name = "Adwaita";
   gtk.iconTheme.package = pkgs.gnome.adwaita-icon-theme;
-  gtk.theme.name = "Adw-gtk3";
-  gtk.theme.package = pkgs.adw-gtk3;
 
+  # TODO: Fix setting GTK theme forcing either light or dark mode for GTK2/3, Electron.js, QT apps.
+  #gtk.theme.name = "Adw-gtk3";
+  #gtk.theme.package = pkgs.adw-gtk3;
+
+  home.packages = [
+    pkgs.adw-gtk3
+  ];
 }
