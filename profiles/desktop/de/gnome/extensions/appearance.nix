bf6da6fefcfd86d82c93e6830c869b98f8581ec5
Sam Lehman <slehman@piwine.com>
2023-04-11 11:12:23 -0400

    profiles.gnome.extensions: add appearance extensions

--- profiles/desktop/de/gnome/extensions/appearance.nix
+++ profiles/desktop/de/gnome/extensions/appearance.nix
@@ -11,9 +11,9 @@
     burn-my-windows            # Change window open/close animations
     #compact-top-bar           # Slimmer top bar
     compiz-windows-effect      # Jiggle windows
-    gnome-bedtime              # Smooth transition to grayscale as bedtime approaches
+    #gnome-bedtime              # Smooth transition to grayscale as bedtime approaches
     gradient-top-bar           # Gradient top bar background
-    material-you-color-theming # Material You palettes from wallpaper applied to libadwaita (breaks light/dark theming)
+    #material-you-color-theming # Material You palettes from wallpaper applied to libadwaita (breaks light/dark theming)
     rounded-window-corners     # Rounding for window corners (fix inconsistency w/ non-libadwaita apps)
     #snowy                     # Snow effect on your desktop
     #transparent-shell         # Transparent top bar, dash, workspace view
