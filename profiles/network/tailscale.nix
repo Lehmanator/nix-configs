678a553873a2fc4f139c46cfd996d5c22ca10942
Sam Lehman <slehman@piwine.com>
2023-04-27 12:38:09 -0400

    profiles.network.tailscale: punch hole in firewall for Tailscale interface, add package for users.

--- profiles/network/tailscale.nix
+++ profiles/network/tailscale.nix
@@ -10,6 +10,10 @@
   imports = [
   ];
 
+  # Make the Tailscale command usable to users
+  environment.systemPackages = [ pkgs.tailscale ];
+
+  # Enable service: tailscaled
   services.tailscale = {
     enable = true;
     interfaceName = "tailscale0";
@@ -18,4 +22,20 @@
     port = 41641;
     useRoutingFeatures = "both";   # Enable subnet routers & exit nodes.  (none|client|server|both)
   };                               #  Need to call `sudo tailscale up` w/ flags `--advertise-exit-node` & `--exit-node`
+
+  # Use MagicDNS
+  # TODO: Make sure nameserver is always first: 100.100.100.100
+  networking.nameservers = [
+    "100.100.100.100"
+    "1.1.1.1"
+  ];
+  networking.search = [
+    "tail6a8f8.ts.net"
+  ];
+
+  # Always allow traffic from Tailscale network
+  networking.firewall.trustedInterfaces = [ config.services.tailscale.interfaceName ];
+
+  # Allow Tailscale ports through the firewall
+  networking.firewall.allowedUDPPorts = [ config.services.tailscale.port ];
 }
