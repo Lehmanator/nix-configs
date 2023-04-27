e76583e9f6c505491cc66fa1a530c27095536bb7
Sam Lehman <slehman@piwine.com>
2023-04-27 12:45:48 -0400

    profiles.desktop.wayland: add package wl-clipboard, set env var to force electron apps to use Ozone platform on NixOS.

--- profiles/desktop/wayland.nix
+++ profiles/desktop/wayland.nix
@@ -1,19 +1,21 @@
-{ self, system, userPrimary, inputs, config, lib, pkgs, ... }:
-let
-in
+{ self, system, userPrimary, inputs, config, lib, pkgs,
+  host, user, repo, network, machine,
+  ...
+}:
 {
   imports = [
     ./xserver.nix
   ];
-  
-  environment.systemPackages = with pkgs; [
-    wl-clipboard
-  ];
-  
+
+  environment.systemPackages = [ pkgs.wl-clipboard ];
+
   hardware.opengl.enable = true;
 
   #programs.wshowkeys.enable = true;
 
   services.xserver.displayManager.gdm.wayland = true;
   services.xserver.windowManager.qtile.backend = "wayland";
+
+  # Try to force Electron apps to use Wayland
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
