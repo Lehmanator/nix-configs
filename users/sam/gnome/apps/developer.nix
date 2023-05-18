461ef699df255d9960e8be7081b452f3f45cc13d
Sam Lehman <slehman@piwine.com>
2023-05-17 21:47:05 -0400

    users.profiles.gnome: add devtools

--- users/sam/gnome/apps/developer.nix
+++ users/sam/gnome/apps/developer.nix
@@ -8,7 +8,14 @@
   ];
 
   home.packages = [
+    pkgs.blackbox-terminal
     pkgs.cambalache
     pkgs.gnome-builder
+    pkgs.gnome-doc-utils
+    pkgs.gnome.dconf-editor
+    pkgs.gnome.devhelp
+    pkgs.gnome.ghex
+    pkgs.gnome.zenity
+
   ];
 }
