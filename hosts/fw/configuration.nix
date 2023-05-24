5298d49d0dbaf3a12903596de28331d08c644ea0
Sam Lehman <github@samlehman.dev>
2023-05-24 19:45:48 -0400

    hosts.fw: import dirs without specifying default.nix

--- hosts/fw/configuration.nix
+++ hosts/fw/configuration.nix
@@ -1,12 +1,17 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{ self, inputs,
-  host, network, repo,
-  config, lib, pkgs,
-  system ? "x86_64-linux",
-  userPrimary ? "sam",
-  ...
+{ self
+, inputs
+, host
+, network
+, repo
+, config
+, lib
+, pkgs
+, system ? "x86_64-linux"
+, userPrimary ? "sam"
+, ...
 }:
 {
   imports = [
@@ -21,8 +26,8 @@
     # Activate profiles
     ../../profiles/activedirectory/admin.nix
     ../../profiles/adb.nix
-    ../../profiles/desktop/default.nix
-    ../../profiles/desktop/de/gnome/default.nix
+    ../../profiles/desktop
+    ../../profiles/desktop/de/gnome
     ../../profiles/desktop/pipewire.nix
     ../../profiles/desktop/flatpak.nix
     ../../profiles/hardware/fprintd.nix
