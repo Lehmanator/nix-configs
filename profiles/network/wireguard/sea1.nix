43ffe4e027eae2407d3a93725056a536a157e329
Sam Lehman <github@samlehman.dev>
2023-05-31 14:44:23 -0400

    profiles.network.wireguard: replace pkgs.helm w/ pkgs.kubernetes-helm (audio program instead of k8s package manager)

--- profiles/network/wireguard/sea1.nix
+++ profiles/network/wireguard/sea1.nix
@@ -38,7 +38,7 @@ let
 
   peer-admin = {
     allowedIPs = [ "45.42.244.62/32" "2602:fcc3::c00b/128" ];
-    endpoint = "45.42.244.130:51420";  # 
+    endpoint = "45.42.244.130:51420";  #
     persistentKeepalive = 10;          # 25
     publicKey = "xTIBA5rboUvnH4htodjb6e697QjLERt1NAB4mZqp8Dg=";
   };
@@ -106,13 +106,13 @@ in
     # Seattle Cluster Administration
     wg-sea1-admin = {
       allowedIPsAsRoutes     = true;   # default: true
-      generatePrivateKeyFile = false;  # default: 
+      generatePrivateKeyFile = false;  # default:
       privateKeyFile = "${secretsDir}/sea1-admin.privkey";
       ips = [ "192.168.123.1/24" ];
       listenPort = 42270;              # default: null  (51820)
       peers = [{
         allowedIPs = [ "45.42.244.62/32" "2602:fcc3::c00b/128" ];
-        endpoint = "45.42.244.130:51420";  # 
+        endpoint = "45.42.244.130:51420";  #
         persistentKeepalive = 10;          # 25
         publicKey = "/XgG41yW5zjQvLJl3D6LKVx5UGMb5vmvQc5GHeW/oFc=";
         #publicKey = "xTIBA5rboUvnH4htodjb6e697QjLERt1NAB4mZqp8Dg=";
@@ -124,6 +124,7 @@ in
   environment.systemPackages = [
     pkgs.kubectl
     pkgs.k9s
-    pkgs.helm
+    pkgs.kubernetes-helm
+    #pkgs.helm  # Some audio program?
   ];
 }
