38b5be4fc1c63b44142199a0462444239a7e25d2
Sam Lehman <github@samlehman.dev>
2023-06-22 13:49:54 -0400

    Revert "users.profiles.devices:"
    This reverts commit ad4599244a44695fa4e81f12c8be1826f46a3002.

--- users/sam/devices/sawfish.nix
+++ users/sam/devices/sawfish.nix
@@ -30,15 +30,11 @@
 
   home.packages = [
     pkgs.android-tools  # Android SDK platform tools (ADB / fastboot)
-    #pkgs.python311Packages.mpd2    # Python MPD lib
-    #pkgs.python311Packages.pydbus  # Python D-Bus lib | dbus-python dbus-fast
-    #pkgs.python311Packages.pyowm   # Python wrapper around OpenWeatherMap web API
-    pkgs.python310Packages.mpd2    # Python MPD lib
-    pkgs.python310Packages.pydbus  # Python D-Bus lib | dbus-python dbus-fast
-    pkgs.python310Packages.pyowm   # Python wrapper around OpenWeatherMap web API
+    pkgs.python311Packages.mpd2    # Python MPD lib
+    pkgs.python311Packages.pydbus  # Python D-Bus lib | dbus-python dbus-fast
+    pkgs.python311Packages.pyowm   # Python wrapper around OpenWeatherMap web API
     pkgs.yarn
     pkgs.dhcp  # Provides dhclient?
-    pkgs.dialog
   ];
 
   # TODO: Build fastboot image: [asteriod-image-sawfish.ext4](https://release.asteroidos.org/nightlies/sawfish/asteroid-image-sawfish.ext4)
@@ -48,6 +44,4 @@
   # TODO: Create package/app:   [atx/AsteroidOSLinux](https://github.com/atx/AsteroidOSLinux)
   #  - Deps: pydbus, python-mpd2, pyown
   # TODO: Create package: [AsteriodOS/asteroid-ctrl](https://github.com/AsteroidOS/asteroid-ctrl)
-  # TODO: Install unofficial-watchfaces
-  # - Deps: dialog or whiptail
 }
