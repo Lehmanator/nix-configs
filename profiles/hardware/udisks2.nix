7fdccaa85f055a54df4ae757fa733ba29a4ed89d
Sam Lehman <github@samlehman.dev>
2023-07-11 18:50:28 -0400

    add profiles: nixos.hardware.udisks, user.desktop.udiskie

---
+++ profiles/hardware/udisks2.nix
@@ -0,0 +1,27 @@
+{ inputs, self
+, config, lib, pkgs
+, ...
+}:
+{
+  imports = [
+  ];
+
+  services.udisks2 = {
+    enable = true;
+    # See:
+    # https://manpages.ubuntu.com/manpages/lunar/en/man8/udisks.8.html
+    # https://manpages.ubuntu.com/manpages/lunar/en/man5/udisks2.conf.5.html
+    settings = {
+      "udisks2.conf" = {
+        defaults.encryption = "luks2";
+      };
+      udisks2 = {
+        modules = [ "*" ];
+        modules_load_preference = "ondemand";
+      };
+    };
+    mountOnMedia = false;  # Mount to /media instead of /run/user/${uid}
+  };
+  #programs.gnome-disks.enable = true;
+}
+
---
+++ users/sam/desktop/udiskie.nix
@@ -0,0 +1,25 @@
+{ inputs, self
+, config, lib, pkgs
+, ...
+}:
+# udiskie: Userspace mount daemon
+# Note: Requires loading ../../../profiles/hardware/udisks2.nix
+{
+  imports = [
+  ];
+
+  services.udiskie = {
+    enable = true;
+    automount = true;
+    notify = true;
+    tray = "auto";
+    settings = {
+      program_options = {
+        udisks_version = 2;
+        tray = true;
+      };
+      icon_names.media = [ "media"  #"media-optical"
+      ];
+    };
+  };
+}
