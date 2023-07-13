140cf9c51aa58c80d437a53a05024b838dc89292
Sam Lehman <github@samlehman.dev>
2023-07-13 13:34:22 -0400

    profile user.shell.common.fzf: add systemctl viewer sysz, tmux integration, & file formatting

--- users/sam/shell/common/fzf.nix
+++ users/sam/shell/common/fzf.nix
@@ -2,20 +2,61 @@
 , config, lib, pkgs
 , ...
 }:
+let
+  border = true;
+  height = "40%";
+in
 {
   imports = [
   ];
 
   programs.fzf = {
     enable = true;
-    changeDirWidgetCommand = "fd --type d";
-    changeDirWidgetOptions = [ "--previeww 'tree -C {} | head -200'" ];
-    #colors = { };
+
+    #colors = {
+    #  bg = "#1e1e1e";
+    #  "bg+" = "#1e1e1e";
+    #  fg = "#d4d4d4";
+    #  "fg+" = "#d4d4d4";
+    #};
+
     #defaultCommand = "fd --type f";
-    defaultOptions = [ "--height 40%" "--border" ];
+    defaultOptions = [
+      "--height ${height}" "--border"
+    ];
+
+    changeDirWidgetCommand = "fd --type d";
+    changeDirWidgetOptions = [
+      "--preview 'tree -C {} | head -200'"
+    ];
+
     fileWidgetCommand = "fd --type f";
-    fileWidgetOptions = [ "--preview 'head {}'" ];
-    #historyWidgetOptions = [ ]; #--sort --exact
+    fileWidgetOptions = [
+      "--preview 'head {}'"
+    ];
+
+    #historyWidgetCommand = "";
+    #historyWidgetOptions = [
+    #  "--sort" "--exact"
+    #];
+
+    tmux = {
+      enableShellIntegration = true;
+      shellIntegrationOptions = [
+        "-d ${height}"
+      ];
+    };
   };
 
+  programs.kakoune.plugins = [ pkgs.kakounePlugins.kak-fzf ];
+
+  #programs.pet = {
+  #  selectcmdPackage = pkgs.fzf;
+  #  settings = {
+  #  };
+  #};
+
+  home.packages = [
+    pkgs.sysz  # systemd fzf UI  # TODO: Conditionally load for Linux systems
+  ];
 }
