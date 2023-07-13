6c21a0e28d987ab5350db0f92242f0f162467e6d
Sam Lehman <github@samlehman.dev>
2023-07-13 17:53:58 -0400

    profile: nixos.desktop: enable user fuse, printing

--- profiles/desktop/default.nix
+++ profiles/desktop/default.nix
@@ -13,8 +13,12 @@
 
   boot.kernelPackages = pkgs.linuxPackages_zen;
 
+  programs.fuse.userAllowOther = true;
+
   # Out of Memory
   #systemd.oomd.enable = true;  # default: true
   services.earlyoom.enable = true;
 
+  # Enable CUPS to print documents
+  services.printing.enable = true;
 }
