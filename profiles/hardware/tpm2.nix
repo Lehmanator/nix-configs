3d74c3ef593d2dcb71178c8b4638cd6c67728002
Sam Lehman <github@samlehman.dev>
2023-07-18 17:04:10 -0400

    profile: nixos.hardware.tpm2: disable tpm2.pkcs11 to fix build [FIXME]

--- profiles/hardware/tpm2.nix
+++ profiles/hardware/tpm2.nix
@@ -1,22 +1,47 @@
-{
-  self,
-  system,
-  userPrimary,
-  inputs,
-  config, lib, pkgs,
-  ...
+{ inputs, self
+, config, lib, pkgs
+, user ? "sam"
+, ...
 }:
-let
-in
 {
   imports = [
   ];
 
-  security.tpm2.enable = true;
-  #security.tpm2.abrmd.enable = true;
-  security.tpm2.pkcs11.enable = true;
+  security.tpm2 = {
+    enable = true;
+    #applyUdevRules = true;
 
-  #services.tcsd.enable = true;
+    #abrmd.enable = true;
+    #abrmd.package = pkgs.tpm2-abrmd;
 
-  users.extraGroups.tss = { name = "tss"; members = [ "sam" ]; };
+    pkcs11.enable = lib.mkDefault false;  # Temporarily disable to fix build
+    #pkcs11.package = pkgs.tpm2-pkcs11;
+
+    #tctiEnvironment = {
+    #  enable = true;
+    #  interface = "device";  # device | tabrmd
+    #  deviceConf = "/dev/tpmrm0";
+    #  tabrmdConf = "bus_name=com.intel.tss2.Tabrmd";
+    #};
+
+    #tssGroup = "tss";
+    #tssUser = if config.security.tpm2.abrmd.enable then "tss" else "root";
+  };
+
+  #services.tcsd = {
+  #  enable = true;
+  #
+  #  firmwarePCRs = "0,1,2,3,4,5,6,7";
+  #  kernelPCRs = "8,9,10,11,12";
+  #
+  #  conformanceCred = "${config.services.tcsd.stateDir}/conformance.cert";
+  #  endorsementCred = "${config.services.tcsd.stateDir}/endorsement.cert";
+  #  platformCred = "${config.services.tcsd.stateDir}/platform.cert";
+  #};
+
+  users.extraGroups.tss = {
+    name = "tss";
+    members = [ "sam" ];
+  };
+
 }
