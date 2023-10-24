60067c1992048775a016dfbe9ea448465c2ffce0
Sam Lehman <github@samlehman.dev>
2023-10-24 08:37:13 -0400

    profiles.nixos.network.tailscale: add gnomeExtensions, non-null cert user, remove hard-coded key path

--- profiles/network/tailscale/default.nix
+++ profiles/network/tailscale/default.nix
@@ -14,19 +14,25 @@
 
   # Make the Tailscale command usable to users
   environment.systemPackages = lib.mkIf config.services.xserver.desktopManager.gnome.enable [
-    pkgs.gnomeExtensions.tailscale-status
     pkgs.gnomeExtensions.taildrop-send
+    pkgs.gnomeExtensions.tailscale-qs
+    pkgs.gnomeExtensions.tailscale-status
     #pkgs.trayscale
   ];
 
   # Enable service: tailscaled
   services.tailscale = {
     enable = true;
+    #authKeyFile = "/run/secrets/tailscale0.key";
     interfaceName = "tailscale0";
     package = pkgs.tailscale;
-    permitCertUid = null; # Username/UserID of the user allowed to fetch Tailscale TLS certificates for the node.
+    permitCertUid = user; #null; # Username/UserID of the user allowed to fetch Tailscale TLS certificates for the node.
     port = 41641;
     useRoutingFeatures = "both"; # Enable subnet routers & exit nodes.  (none|client|server|both)
+    extraUpFlags = [
+      "set --operator=${user}"
+      "--ssh"
+    ];
   }; #  Need to call `sudo tailscale up` w/ flags `--advertise-exit-node` & `--exit-node`
 
   networking = {
