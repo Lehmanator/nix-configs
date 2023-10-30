92e018ab1c372e3cd62d0409d9bd2785621100f1
Sam Lehman <github@samlehman.dev>
2023-10-30 11:02:44 -0400

    hosts.fajita: add default.nix

---
+++ hosts/fajita/default.nix
@@ -0,0 +1,28 @@
+{ inputs, self
+, user
+, config, lib, pkgs
+, ...
+}:
+{
+  imports = [
+
+    # System configuration
+    ./configuration.nix
+
+    # Configuration related to hardware
+    #./hardware-configuration.nix
+
+    # Uses some stuff from SnowflakeOS
+    #./snowflake.nix
+
+    # Handles hardware peripherals for external & internal displays
+    #./displays.nix
+
+    # Include configuration managed by apps:
+    # - nixos-conf-editor
+    # - nix-software-center
+    #./managed.nix
+
+  ];
+
+}
