1e4779ad809f6ae0ba358fa2c149036e83322940
Sam Lehman <github@samlehman.dev>
2023-11-28 18:20:49 -0500

    Init sops-nix [WIP]

--- profiles/security/sops.nix
+++ profiles/security/sops.nix
@@ -1,10 +1,40 @@
-{ inputs, self
-, config, lib, pkgs
+{ inputs
+, self
+, config
+, lib
+, pkgs
 , ...
 }:
 {
   # TODO: Make devShell with pkgs.sops installed
-  imports = [inputs.sops-nix.nixosModules.sops];
+  imports = [ inputs.sops-nix.nixosModules.sops ];
+
+  sops = {
+    defaultSopsFile = ../../hosts/${config.networking.hostName}/secrets/default.yaml;
+    age = {
+      generateKey = true;
+      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ]; #"/etc/ssh/ssh_host_rsa_key"];
+      #keyFile = "/var/lib/sops-nix/sops-host-age.privkey";
+      #lib.map (k: k.path) config.services.openssh.hostKeys;  # TODO: Filter to type="rsa"
+    };
+    gnupg = {
+      home = "/var/lib/sops";
+      #home = "/root/.local/share/gnupg";
+      #sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" "/etc/ssh/ssh_host_rsa_key" ];
+    };
+
+    # --- Secrets ---
+    secrets = {
+      #  user-default-password = { group = "users"; };
+      #  user-root-password    = { owner = "root"; group = "root"; };
+      #
+      #  # TODO: Move to NixOS profile for disk encryption
+      #  disk-default-password = { owner = "root"; group = "root"; };
+      #  disk-default-keyfile  = { owner = "root"; group = "root"; };
+      test-host-secret = { };
+    };
+
+  };
 
   # --- Packages ---
   environment.systemPackages = [
@@ -13,28 +43,18 @@
     pkgs.sops
     pkgs.kustomize-sops
     pkgs.terraform-providers.sops
+
+    pkgs.ssh-to-age
+
   ] ++ (with inputs.sops-nix.packages.${pkgs.system}; [
-    #lint
+
     sops-import-keys-hook
     sops-init-gpg-key
     sops-install-secrets
-    sops-pgp-hook
     ssh-to-pgp
+
+    #lint           # Broken build
+    #sops-pgp-hook  # Deprecated
   ]);
-
-  # --- Config ---
-  sops = {
-    defaultSopsFile = ../hosts/${config.networking.hostName}/secrets.yaml;
-
-    # --- Secrets ---
-    secrets = {
-    #  user-default-password = { group = "users"; };
-    #  user-root-password    = { owner = "root"; group = "root"; };
-    #
-    #  # TODO: Move to NixOS profile for disk encryption
-    #  disk-default-password = { owner = "root"; group = "root"; };
-    #  disk-default-keyfile  = { owner = "root"; group = "root"; };
-    };
-  };
 
 }
--- users/sam/sops.nix
+++ users/sam/sops.nix
@@ -1,18 +1,67 @@
-{ inputs, self
-, config, lib, pkgs
+{ inputs
+, self
+, config
+, lib
+, pkgs
+, user
 , ...
 }:
 {
-  imports = [
-    #inputs.sops-nix.homeManagerModules.sops
-  ];
+  #imports = [ inputs.sops-nix.homeManagerModules.sops ];
+
+  sops = {
+    defaultSopsFile = ../../users/${user}/.secrets/default.yaml;
+    keepGenerations = 10;
+
+    age = {
+      generateKey = true;
+      sshKeyPaths = [
+        "${config.home.homeDir}/.ssh/id_ed25519"
+        "${config.home.homeDir}/.ssh/id_sops_ed25519"
+        #"${config.home.homeDir}/.ssh/id_sops_rsa"
+        #"${config.home.homeDir}/.ssh/id_rsa"
+        #"${config.home.homeDir}/.ssh/id_fw_rsa"
+      ];
+      #  "${config.home.homeDir}/.ssh/id_ed25519" ];
+      #keyFile = "${config.home.homeDir}/.local/secrets/sops-age.privkey";
+    };
+
+    gpupg = {
+      home = config.programs.gnupg.homedir;
+      #sshKeyPaths = [
+      #  "${config.home.homeDir}/.ssh/id_rsa"
+      #  "${config.home.homeDir}/.ssh/id_ed25519"
+      #];
+    };
+
+    secrets = {
+      test-user-secret = { };
+    };
+  };
 
   home.packages = [
-    pkgs.ssh-to-pgp
+    #pkgs.ssh-to-pgp
     pkgs.ssh-to-age
     pkgs.ssh-copy-id
     pkgs.ssh-import-id
     pkgs.ssh-key-confirmer
+
     pkgs.autossh
-  ];
+
+    pkgs.age
+    pkgs.rage
+
+    pkgs.sops
+    pkgs.kustomize-sops
+    pkgs.terraform-providers.sops
+
+  ] ++ (with inputs.sops-nix.packages.${pkgs.system}; [
+    sops-import-keys-hook
+    sops-init-gpg-key
+    sops-install-secrets
+    ssh-to-pgp
+
+    #lint           # Broken build
+    #sops-pgp-hook  # Deprecated
+  ]);
 }
