48bbbf146cca8756c8403f38d9d3f374468e0df0
Sam Lehman <github@samlehman.dev>
2023-12-07 15:23:42 -0500

    profiles.nixos.desktop.de.gnome.extensions.appearance: reformat, disable rounded-window-corners

--- profiles/desktop/de/gnome/extensions/appearance.nix
+++ profiles/desktop/de/gnome/extensions/appearance.nix
@@ -1,6 +1,7 @@
-{
-  config, lib, pkgs,
-  ...
+{ config
+, lib
+, pkgs
+, ...
 }:
 {
   imports = [
@@ -8,13 +9,13 @@
 
   environment.systemPackages = with pkgs.gnomeExtensions; [
     #blur-my-shell             # Blur GNOME UI elements
-    burn-my-windows            # Change window open/close animations
+    burn-my-windows # Change window open/close animations
     #compact-top-bar           # Slimmer top bar
-    compiz-windows-effect      # Jiggle windows
+    compiz-windows-effect # Jiggle windows
     #gnome-bedtime              # Smooth transition to grayscale as bedtime approaches
-    gradient-top-bar           # Gradient top bar background
+    gradient-top-bar # Gradient top bar background
     #material-you-color-theming # Material You palettes from wallpaper applied to libadwaita (breaks light/dark theming)
-    rounded-window-corners     # Rounding for window corners (fix inconsistency w/ non-libadwaita apps)
+    #rounded-window-corners     # Rounding for window corners (fix inconsistency w/ non-libadwaita apps) (11/23 - no GNOME 45 compat)
     #snowy                     # Snow effect on your desktop
     #transparent-shell         # Transparent top bar, dash, workspace view
   ];
