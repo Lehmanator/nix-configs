51a09a49d55ce3ef01ecc2bbc9eb08dff93f7e3b
Sam Lehman <github@samlehman.dev>
2023-12-26 11:38:49 -0500

    hosts.fw: remove snowflake config. [WIP]

--- hosts/fw/default.nix
+++ hosts/fw/default.nix
@@ -3,7 +3,7 @@
   imports = [
     ./configuration.nix # System configuration
     #./hardware-configuration.nix # Configuration related to hardware
-    ./snowflake.nix # Uses some stuff from SnowflakeOS
+    #./snowflake.nix # Uses some stuff from SnowflakeOS
     ./displays.nix # Handles hardware peripherals for external & internal displays
     ./managed.nix # Include app-managed config: nixos-conf-editor & nix-software-center
     ./profiles.nix # Include imported profiles
