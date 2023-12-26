a871f4a760e6d2c478fca600b4ce59f286d0c4d7
Sam Lehman <github@samlehman.dev>
2023-12-26 11:35:19 -0500

    hosts.wyse: update disko config RAM, swap, partition attrset

--- hosts/wyse/disko.nix
+++ hosts/wyse/disko.nix
@@ -1,32 +1,34 @@
 { inputs, ... }: {
   disko.devices = {
-    nodev."/" = { fsType = "tmpfs"; mountOptions = [ "defaults" "size=3G" "mode=755" ]; };
+    nodev."/" = { fsType = "tmpfs"; mountOptions = [ "defaults" "size=8G" "mode=755" ]; };
     disk.main = {
       type = "disk";
       device = "/dev/nvme0n1";
       content = {
         type = "gpt";
-        partitions.ESP = {
-          size = "1024M";
-          type = "EF00";
-          content = { mountpoint = "/boot"; mountOptions = [ "defaults" ]; format = "vfat"; type = "filesystem"; };
-        };
-        partitions.luks = {
-          size = "100%";
-          content = {
-            type = "luks";
-            name = "crypted";
-            askPassword = true;
-            settings.allowDiscards = true;
+        partitions = {
+          ESP = {
+            size = "1024M";
+            type = "EF00";
+            content = { mountpoint = "/boot"; mountOptions = [ "defaults" ]; format = "vfat"; type = "filesystem"; };
+          };
+          luks = {
+            size = "100%";
             content = {
-              type = "btrfs";
-              extraArgs = [ "-f" ];
-              subvolumes = {
-                "/nix" = { mountpoint = "/nix"; mountOptions = [ "compress=zstd" "noatime" ]; };
-                "/persist" = { mountpoint = "/nix/persist"; mountOptions = [ "compress=zstd" "noatime" "noexec" ]; };
-                "/home" = { mountpoint = "/home"; mountOptions = [ "compress=zstd" "noatime" ]; };
-                "/swap" = { mountpoint = "/swap"; swap.swapfile.size = "24G"; };
-                "/var" = { mountpoint = "/var"; mountOptions = [ "default" ]; };
+              type = "luks";
+              name = "crypted";
+              askPassword = true;
+              settings.allowDiscards = true;
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = {
+                  "/nix" = { mountpoint = "/nix"; mountOptions = [ "compress=zstd" "noatime" ]; };
+                  "/persist" = { mountpoint = "/nix/persist"; mountOptions = [ "compress=zstd" "noatime" "noexec" ]; };
+                  "/home" = { mountpoint = "/home"; mountOptions = [ "compress=zstd" "noatime" ]; };
+                  "/swap" = { mountpoint = "/nix/swap"; swap.swapfile.size = "32G"; };
+                  "/var" = { mountpoint = "/var"; mountOptions = [ "default" ]; };
+                };
               };
             };
           };
