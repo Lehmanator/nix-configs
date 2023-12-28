9f1bafff3f82a4d6564604c719c59e85695f9a91
Sam Lehman <github@samlehman.dev>
2023-12-27 23:23:24 -0500

    nixos.hardware.display.displaylink: update url+hash, add evdi kernel module, add displaylink package.

--- nixos/profiles/hardware/display/displaylink.nix
+++ nixos/profiles/hardware/display/displaylink.nix
@@ -5,13 +5,19 @@
 , ...
 }:
 # Note: Must run the following to download unfree blobs for DisplayLink
+
+# https://www.synaptics.com/sites/default/files/exe_files/2023-08/DisplayLink%20USB%20Graphics%20Software%20for%20Ubuntu5.8-EXE.zip
+# hash = "05m8vm6i9pc9pmvar021lw3ls60inlmq92nling0vj28skm55i92";
+
 # $ nix-prefetch-url --name displaylink-<VersionNoSpecialChars>.zip https://www.synaptics.com/sites/default/files/exe_files/2023-04/DisplayLink%20USB%20Graphics%20Software%20for%20Ubuntu5.7-EXE.zip
 #   Old: nix-prefetch-url --name displaylink.zip https://www.synaptics.com/sites/default/files/exe_files/2023-04/DisplayLink%20USB%20Graphics%20Software%20for%20Ubuntu5.7-EXE.zip
 #   Note: NixOS expects filename=displaylink-570.zip, not displaylink.zip
 #     nix-prefetch-url --name displaylink-570.zip https://www.synaptics.com/sites/default/files/exe_files/2023-04/DisplayLink%20USB%20Graphics%20Software%20for%20Ubuntu5.7-EXE.zip
 {
-  imports = [
-  ];
+  imports = [ ];
+
+  boot.kernelModules = [ "evdi" ];
+  environment.systemPackages = [ pkgs.displaylink ];
 
   services.xserver.videoDrivers = [ "displaylink" "modesetting" ];
 
