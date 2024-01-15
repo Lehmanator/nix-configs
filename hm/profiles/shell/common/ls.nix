c3211b1fb373081a3052c492b584f38c08d87101
Sam Lehman <github@samlehman.dev>
2024-01-15 09:41:53 -0500

    hm.shell: create profile pls, import in profile ls. [WIP]

--- hm/profiles/shell/common/ls.nix
+++ hm/profiles/shell/common/ls.nix
@@ -1,12 +1,14 @@
-{ inputs
-, config, lib, pkgs
-, ...
-}:
-let
+{
+  inputs,
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
   program = "eza";
   readable = {
-    size  = true;
-    date  = true;
+    size = true;
+    date = true;
     perms = true;
   };
   sort = {
@@ -23,22 +25,24 @@ let
   show = {
     classify = false;
     color = true;
-    git   = true;
+    git = true;
     icons = true;
     headers = true;
     links = true;
     symlinks = true;
     total = true;
   };
-in
-{
+in {
   imports = [
+    #./eza.nix
+    #./lsd.nix
+    ./pls.nix
   ];
 
   # --- Directory Listing -----
   home.shellAliases = {
-    l = "${program} -a";
-    lt = "${program} --tree";
+    l = lib.mkDefault "${program} -a";
+    lt = lib.mkDefault "${program} --tree";
   };
 
   # Sort Fields:
@@ -67,11 +71,28 @@ in
     icons = show.icons;
     extraOptions = [
       "--level=${builtins.toString view.depth}"
-      (if show.headers  then "--header"                  else " ")
-      (if show.color    then "--color-scale"             else "")
+      "--hyperlink"
+      (
+        if show.headers
+        then "--header"
+        else " "
+      )
+      (
+        if show.color
+        then "--color-scale=gradient"
+        else ""
+      )
       #(if show.classify then "--classify"                else "")
-      (if sort.ignored  then "--git-ignore"              else "")
-      (if sort.dirs == "first" then "--group-directories-first" else "")
+      (
+        if sort.ignored
+        then "--git-ignore"
+        else ""
+      )
+      (
+        if sort.dirs == "first"
+        then "--group-directories-first"
+        else ""
+      )
     ];
   };
 
@@ -79,7 +100,8 @@ in
   programs.lsd = {
     enable = true;
     enableAliases = program == "lsd";
-    settings = {  # See: https://github.com/Peltoche/lsd#config-file-content
+    settings = {
+      # See: https://github.com/Peltoche/lsd#config-file-content
       blocks = [
         "permission"
         "user"
@@ -90,30 +112,55 @@ in
         #"context"
         #"inode"
         #"links"
-        (if show.git then "git" else "") #"git"
+        (
+          if show.git
+          then "git"
+          else ""
+        ) # "git"
       ];
-      classic     = false;
-      color.theme = "default";                                            # default  | custom (looks for ~/.config/lsd/colors.yaml)
-      color.when  = if show.color     then "auto"       else "never";     # always   | auto   | never
-      hyperlink   = if show.links     then "auto"       else "never";     # always   | auto   | never
-      icons.when  = if show.icons     then "auto"       else "never";     # always   | auto   | never
-      icons.theme = if show.icons     then "fancy"      else "unicode";   # unicode  | fancy
-      date        = if readable.date  then "relative"   else "date";      # relative | date   | +<date_format>
-      permission  = if readable.perms then "rwx"        else "octal";     # octal    | rwx
-      size        = if readable.size  then "short"      else "default";   # default  | short  | bytes
-      recursion.enabled    = view.recurse;
-      recursion.depth      = view.depth;
-      layout               = view.layout;     # grid  | tree | oneline
-      sorting.column       = sort.field;      # name  | time | size    | version
-      sorting.dir-grouping = sort.dirs;       # first | last | none
-      sorting.reverse      = sort.reverse;
-      indicators           = show.classify;
-      header               = show.headers;
-      total-size           = show.total;  # Show total size of directories. Default=false
-      ignore-globs = [ ".git" ".hg" ".svn" ];
+      classic = false;
+      color.theme = "default"; # default  | custom (looks for ~/.config/lsd/colors.yaml)
+      color.when =
+        if show.color
+        then "auto"
+        else "never"; # always   | auto   | never
+      hyperlink =
+        if show.links
+        then "auto"
+        else "never"; # always   | auto   | never
+      icons.when =
+        if show.icons
+        then "auto"
+        else "never"; # always   | auto   | never
+      icons.theme =
+        if show.icons
+        then "fancy"
+        else "unicode"; # unicode  | fancy
+      date =
+        if readable.date
+        then "relative"
+        else "date"; # relative | date   | +<date_format>
+      permission =
+        if readable.perms
+        then "rwx"
+        else "octal"; # octal    | rwx
+      size =
+        if readable.size
+        then "short"
+        else "default"; # default  | short  | bytes
+      recursion.enabled = view.recurse;
+      recursion.depth = view.depth;
+      layout = view.layout; # grid  | tree | oneline
+      sorting.column = sort.field; # name  | time | size    | version
+      sorting.dir-grouping = sort.dirs; # first | last | none
+      sorting.reverse = sort.reverse;
+      indicators = show.classify;
+      header = show.headers;
+      total-size = show.total; # Show total size of directories. Default=false
+      ignore-globs = [".git" ".hg" ".svn"];
 
       # --- Symlinks ---
-      dereference = true; #!show.symlinks;
+      dereference = true; # !show.symlinks;
       no-symlink = !show.symlinks;
       symlink-arrow = "⇒";
     };
