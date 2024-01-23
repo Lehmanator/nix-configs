34d55ed8a47bacdec4e266972891f95b6ebb7437
Sam Lehman <github@samlehman.dev>
2024-01-23 07:29:31 -0500

    hm.pager: add bat + bat-utils & make zsh.shellGlobalAliases + home.shellAliases

--- hm/profiles/pager/default.nix
+++ hm/profiles/pager/default.nix
@@ -1,8 +1,10 @@
-{ self, inputs
-, config, lib, pkgs
-, ...
-}:
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  # TODO: Split into ./less.nix, ./bat.nix
   imports = [
     #inputs.home-extra-xhmm.homeManagerModules.console.less
   ];
@@ -10,29 +12,41 @@
   #programs.less.options = [ #types.str
   #];
 
-  programs.bat = {
-    enable = true;
-    extraPackages = with pkgs.bat-extras; [
-      batdiff
-      batman
-      batgrep
-      batwatch
-    ];
-    config = {
-      map-syntax = [
-        "*.jenkinsfile:Groovy"
-        "*.props:Java Properties"
-      ];
-      pager = "less -FR";
-      theme = "Monokai Extended Light";
-      #theme = "TwoDark";
+  programs = {
+    bat = {
+      enable = true;
+      extraPackages = with pkgs.bat-extras; [batdiff batman batgrep batwatch];
+      config = {
+        map-syntax = ["*.jenkinsfile:Groovy" "*.props:Java Properties"];
+        pager = "less -FR";
+        theme = "Monokai Extended Light";
+        #theme = "TwoDark";
+      };
+      themes = {};
     };
-    themes = { };
+    zsh.shellGlobalAliases = {
+      BAT = "| bat";
+      BCAT = "| bat";
+      BDIFF = "| batdiff";
+      BGREP = "| batgrep";
+      BMAN = "| batman";
+      BWATCH = "| batwatch";
+    };
   };
 
-  home.shellAliases.b = "bat";
-  programs.zsh.shellGlobalAliases = {
-    BAT = "| bat";
-    B = "| bat";
+  home = {
+    sessionVariables.MANPAGER =
+      lib.mkIf config.programs.bat.enable "sh -c 'col -bx | bat -l man -p'";
+    shellAliases = {
+      b = "bat";
+      bcat = "bat";
+      bdiff = "batdiff";
+      bgrep = "batgrep";
+      bman = "batman";
+      bwatch = "batwatch";
+      batfzfpreview =
+        lib.mkIf config.programs.fzf.enable
+        "bat --color=always --style=numbers --line-range=:5000";
+    };
   };
 }
