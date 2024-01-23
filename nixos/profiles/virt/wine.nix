663fb344d6c70577259a38ffd6be53e3a12762ca
Sam Lehman <github@samlehman.dev>
2024-01-23 07:10:01 -0500

    profiles.nixos.virt.wine: remove broken pkgs: playonlinux, winetricks, wineWowPackages.waylandFull

--- nixos/profiles/virt/wine.nix
+++ nixos/profiles/virt/wine.nix
@@ -1,14 +1,18 @@
-{ inputs, config, lib, pkgs, user, ... }: {
-  imports = [ ];
-  environment.systemPackages = with pkgs; [
-    bottles
-    lutris
-    #onedrive
-    playonlinux
-    proton-caller
-    protonup-qt
-    protontricks
-    wineWowPackages.waylandFull # TODO: Conditional based on wayland support
-    winetricks
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  environment.systemPackages = [
+    pkgs.bottles
+    pkgs.lutris
+    pkgs.onedrive
+    #pkgs.playonlinux
+    pkgs.proton-caller
+    pkgs.protonup-qt
+    pkgs.protontricks
+    #pkgs.wineWowPackages.waylandFull # Broken: 2023-01-23  # TODO: Conditional based on wayland support
+    #pkgs.winetricks # Broken: 2023-01-23
   ];
 }
