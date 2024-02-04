f05904dc6b74aa1a15b607bf922598275a78192d
Sam Lehman <github@samlehman.dev>
2024-02-04 02:46:19 -0500

    hm.cachix-agent: named agent

--- hm/profiles/cachix-agent.nix
+++ hm/profiles/cachix-agent.nix
@@ -2,9 +2,11 @@
   config,
   lib,
   pkgs,
+  user,
   ...
 }: {
   services.cachix-agent = {
+    name = "home-manager-${user}";
     enable = true;
     #credentialsFile = "${config.sops.secrets.cachix-agent-token.path}";
   };
