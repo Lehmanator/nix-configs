06ed77087851683e58ee2339c89bf4b9fa91de5a
Sam Lehman <github@samlehman.dev>
2024-02-08 04:54:12 -0500

    nixos.shell: cleanup default, fzf, zsh

--- nixos/profiles/shell/default.nix
+++ nixos/profiles/shell/default.nix
@@ -1,46 +1,25 @@
-{ config
-, lib
-, pkgs
-, user
-, ...
-}:
 {
-  imports = [
-    #./nushell.nix
-    ./zsh.nix
-  ];
-
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = [./zsh.nix];
   environment = {
-    #sessionVariables.ZDOTDIR = "$HOME/.config/zsh";  # TODO: Use sessionVariables?
-    pathsToLink = [ "/share/bash-completion" ]; #                 # Link bash completions. Used by bash & ZSH compat.
-    systemPackages = [ pkgs.unixODBC ]; #                         # Package to get CLI clients to connect to ODBC databases.
-    unixODBCDrivers = with pkgs.unixODBCDrivers; [ sqlite psql ]; # Unix ODBC drivers to register in /etc/odbcinst.ini
+    # Link bash completions. Used by bash & ZSH compat.
+    pathsToLink = ["/share/bash-completion" "/share/zsh"];
 
-    # --- Shell Initialization ---
-    #interactiveShellInit = "";
-    #loginShellInit = "neofetch";
-    #shellInit = "";
+    # Package to get CLI clients to connect to ODBC databases.
+    systemPackages = [pkgs.bash pkgs.zsh pkgs.nushell pkgs.unixODBC];
 
-    # --- Shell Aliases ---
+    # Unix ODBC drivers to register in /etc/odbcinst.ini
+    unixODBCDrivers = [pkgs.unixODBCDrivers.sqlite pkgs.unixODBCDrivers.psql];
+
     shellAliases.ctl = "systemctl";
+    shells = [pkgs.bash pkgs.zsh pkgs.nushell];
   };
 
-  # --- Prompt -------------------------------------------------------
-  #programs.starship = { # Prompt for Bash & ZSH
-  #  enable = true;
-  #  interactiveOnly = false;  # Some plugins require this to be set to false to function correctly. Default=true
-  #  #settings = {};
-  #};
-
-  # --- Shell Utils ----------------------------------------
   # --- command-not-found ---
   # TODO: Find nix-related program from old config that was better
-  # - manix ?
+  # - manix, comma ?
   programs.command-not-found.enable = lib.mkDefault false;
-
-  # --- thefuck: command corrector ---
-  #programs.thefuck = {
-  #  enable = true;
-  #  alias = "fuck";
-  #};
 }
