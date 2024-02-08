2eb70a2d5842ccf9117a6113862cb8073f98b586
Sam Lehman <github@samlehman.dev>
2024-02-08 18:00:33 -0500

    parts.ez-configs: fajita

--- parts/ez-configs.nix
+++ parts/ez-configs.nix
@@ -8,16 +8,18 @@
   # TODO: Restructure dirs
   ezConfigs = {
     globalArgs = {
-      inherit inputs;
+      inherit inputs self;
       user = "sam";
     };
     home = {
       configurationsDirectory = "../hm/users";
       modulesDirectory = "../hm/modules";
+      extraSpecialArgs = {};
     };
     nixos = {
       modulesDirectory = "../nixos/profiles";
       configurationsDirectory = "../nixos/hosts";
+      hosts = {fajita = {system = "aarch64-linux";};};
     };
     darwin = {
       modulesDirectory = "../darwin/profiles";
