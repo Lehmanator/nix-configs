be0540c2ca6fe77455da22c978836e97828ffd61
Sam Lehman <github@samlehman.dev>
2024-02-08 11:16:20 -0500

    common.shell.scripts: init w/ terminal-helpers [WIP]

---
+++ common/profiles/shell/scripts/terminal-helpers.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  # TODO: Test terminal features (bold, underlines, italics, strikethrough, blinking, etc.)
+  # TODO: Test terminal colors
+  # TODO: Test terminal fonts
+  # - Powerline Symbols
+  # - Ligatures
+  # - Emoji
+  # - Unicode special chars used for UI
+  # - Box-drawing
+  # - Hard-to-distinguish characters
+  #   - o / O / 0 / Q
+  #   - o / a
+  #   - L / l / 1 / i / I / |
+  #   - . / ,
+
+  # TODO: Clear terminal
+  # TODO: Pretty print `$PATH`, `$fpath`, `$NIX_PATH`, etc.
+  # TODO: Pretty print all `$XDG_*` variables
+  # TODO: Show shell keybindings
+  # TODO: Show shell zstyles
+  # TODO: Show shell completions
+
+}
