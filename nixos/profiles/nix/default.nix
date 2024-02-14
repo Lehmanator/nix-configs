ae9d9c2bcccbc12a890b992dd9ef1ba6888babc5
Sam Lehman <github@samlehman.dev>
2024-02-14 04:17:02 -0500

    nixos.nix: nix.settings.nix-path use list instead of str

--- nixos/profiles/nix/default.nix
+++ nixos/profiles/nix/default.nix
@@ -15,13 +15,14 @@
   ];
 
   # Set NIX_PATH to contain all my flake inputs (with nixpkgs & config sorted first)
-  nix.settings.nix-path = with lib.lists;
-    lib.intersperse ":" (builtins.map (i: "${i}=/etc/nix/inputs/${i}")
-      (["nixpkgs" "self"]
-        ++ (remove "nixpkgs" (remove "self" (builtins.attrNames inputs)))));
+  nix.settings.nix-path = with lib.lists; (builtins.map (i: "${i}=/etc/nix/inputs/${i}") (["nixpkgs" "self"]
+      ++ (remove "nixpkgs" (remove "self" (builtins.attrNames inputs)))));
 
   environment = {
     extraOutputsToInstall = ["bin"]; # [ "doc" "info" "devdoc" "dev" "bin" ];
+    #sessionVariables.NIX_PATH = lib.intersperse ":" config.nix.settings.nix-path;
+    #sessionVariables.NIX_PATH = with lib.lists; lib.intersperse ":" (builtins.map (i: "${i}=/etc/nix/inputs/${i}")
+    #  (["nixpkgs" "self"] ++ (remove "nixpkgs" (remove "self" (builtins.attrNames inputs)))));
     shellAliases = {
       nix-closure-list = "nix-store -qR `which $1`"; # TODO: Figure out how to allow
       nix-closure-tree = "nix-store -q --tree `which $1`"; # arg not at end of alias
