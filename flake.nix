44b171a0cc714cbf3fd5eaa065693f4cd92d8ab8
Sam Lehman <github@samlehman.dev>
2024-02-14 04:31:30 -0500

    flake: add lehmanator.cachix.org to nixConfig.substituters. rename nixConfig.trusted-keys -> trusted-public-keys

--- flake.nix
+++ flake.nix
@@ -212,10 +212,15 @@
 
   nixConfig = {
     connect-timeout = 10;
-    substituters = ["https://cache.nixos.org/" "https://nix-community.cachix.org/"];
-    trusted-keys = [
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org/"
+      "https://lehmanator.cachix.org/"
+    ];
+    trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "lehmanator.cachix.org-1:kT+TO3tnSoz+lxk2YZSsMOtVRZ7Gc57jaKWL57ox1wU="
     ];
   };
 
