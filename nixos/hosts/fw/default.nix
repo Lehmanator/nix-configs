792cb139704f5c7b47b5c5a9df8aec93e3ade0de
Sam Lehman <github@samlehman.dev>
2024-02-21 00:24:16 -0500

    hosts.fw: save attempted code. Will rethink & refactor later [WIP]

--- nixos/hosts/fw/default.nix
+++ nixos/hosts/fw/default.nix
@@ -1,5 +1,11 @@
-{ inputs, ... }:
 {
+  inputs,
+  config,
+  pkgs,
+  lib,
+  self,
+  ...
+}: {
   imports = [
     ./configuration.nix # System configuration
     #./hardware-configuration.nix # Configuration related to hardware
@@ -19,4 +25,20 @@
   # https://github.com/taotien/framework_toolbox
   # https://github.com/DHowett/framework-ec
   # https://github.com/morpheus636/awesome-framework
+
+  #isoImage.isoName =
+  #  lib.mkImageMediaOverride
+  #  "lehmanator-${config.system.build.installHostname}-${config.system.nixos.release}-${
+  #    inputs.self.shortRev or "dirty"
+  #  }-${pkgs.stdenv.hostPlatform.uname.processor}.iso";
+  #isoImage.volumeID = "lehmanator-${config.system.nixos.release}-${
+  #  inputs.self.shortRev or "dirty"
+  #}-${pkgs.stdenv.hostPlatform.uname.processor}";
+  #
+  #system.build.installHostname = config.networking.hostName;
+  #system.build.installClosure = config.system.build.toplevel;
+  #system.build.installDiskoScript = config.system.build.diskoScript;
+  #system.build.installer = pkgs.runCommandLocal config.isoImage.isoName {
+  #  isoPath = "${config.system.build.isoImage}/iso/${config.isoImage.isoName}";
+  #} ''ln -s "$isoPath" $out'';
 }
