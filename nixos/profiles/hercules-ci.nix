e06c67614db57f1f9b21fffeda744d53560fc1dc
Sam Lehman <github@samlehman.dev>
2024-02-21 00:23:19 -0500

    nixos.hercules-ci: enable service, load module from input [WIP]

--- nixos/profiles/hercules-ci.nix
+++ nixos/profiles/hercules-ci.nix
@@ -1,11 +1,18 @@
 {
+  inputs,
   config,
   lib,
   pkgs,
   ...
 }: {
-  #imports = [ ./cachix.nix ];
-  services.hercules-ci-agent = {enable = true;};
+  imports = [
+    #./cachix.nix
+    inputs.hercules-ci-agent.nixosModules.agent-service
+  ];
+  services.hercules-ci-agent = {
+    enable = true;
+    #concurrentTasks = 4;
+  };
   sops.secrets.hercules-ci-agent-cluster-join-token = {
     path = "/var/lib/hercules-ci-agent/secrets/cluster-join-token.key";
     owner = "hercules-ci-agent";
