8005f997c38f2ec353b99913c093fe545457eabf
Sam Lehman <github@samlehman.dev>
2024-02-23 00:32:01 -0500

    hosts.wyse.disko: add comment w/ link to swap seetup

--- hosts/wyse/disko.nix
+++ hosts/wyse/disko.nix
@@ -26,6 +26,7 @@
                   "/nix" = { mountpoint = "/nix"; mountOptions = [ "compress=zstd" "noatime" ]; };
                   "/persist" = { mountpoint = "/nix/persist"; mountOptions = [ "compress=zstd" "noatime" "noexec" ]; };
                   "/home" = { mountpoint = "/home"; mountOptions = [ "compress=zstd" "noatime" ]; };
+                  # https://git.dblsaiko.net/systems/tree/configurations/invader/swap.nix
                   "/swap" = { mountpoint = "/nix/swap"; swap.swapfile.size = "32G"; };
                   "/var" = { mountpoint = "/var"; mountOptions = [ "default" ]; };
                 };
