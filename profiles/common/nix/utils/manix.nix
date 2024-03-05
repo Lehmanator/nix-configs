4c11e2e83f1f59baa521d80bca3fd624d639cdd0
Sam Lehman <github@samlehman.dev>
2024-03-05 16:14:22 -0500

    feat: fzf-manix with colored preview of Nix docs.

--- profiles/common/nix/utils/manix.nix
+++ profiles/common/nix/utils/manix.nix
@@ -1,16 +1,25 @@
-{pkgs, ...}: {
+{ config, pkgs, lib, ... }:
+let
+  inherit (lib) getExe getExe' optional;
+  grep = getExe pkgs.ripgrep; # pkgs.ripgrep-all; #pkgs.gnugrep;
+  fzf = getExe pkgs.fzf;
+  manix = getExe pkgs.manix;
+  sed = getExe pkgs.gnused;
+  #xargs = getExe' pkgs.findutils "xargs";
+
+  hasVim = optional (config.programs.vim.defaultEditor
+    || config.programs.neovim.enable
+    || (config.programs ? nixvim && config.programs.nixvim.enable));
+  manix-alias = ''
+    ${manix} "" | ${grep} '^# ' | ${sed} 's/^# (.*) (.*/1/;s/ (.*//;s/^# //' | sort | ${fzf} --preview="${manix} '{}'" | xargs ${manix}'';
+in
+{
   environment = {
-    # TODO: Use ripgrep instead of grep
-    # TODO: Use binary path to manix
-    # TODO: Other manix aliases?
+    systemPackages = [ pkgs.manix ] ++ hasVim pkgs.vimPlugins.telescope-manix;
     shellAliases = {
-      nix-manix = ''
-        manix "" | grep '^# ' | sed 's/^# (.*) (.*/1/;s/ (.*//;s/^# //' | fzf --preview="manix '{}'" | xargs manix'';
-      n-manix = ''
-        manix "" | grep '^# ' | sed 's/^# (.*) (.*/1/;s/ (.*//;s/^# //' | fzf --preview="manix '{}'" | xargs manix'';
-      n-doc = ''
-        manix "" | grep '^# ' | sed 's/^# (.*) (.*/1/;s/ (.*//;s/^# //' | fzf --preview="manix '{}'" | xargs manix'';
+      nix-manix = manix-alias;
+      n-manix = manix-alias;
+      n-doc = manix-alias;
     };
-    systemPackages = [pkgs.manix];
   };
 }
