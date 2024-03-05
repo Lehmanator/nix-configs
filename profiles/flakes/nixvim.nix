3a3a39f00aaec2b29052cd81263165047f6b1cda
Sam Lehman <github@samlehman.dev>
2024-03-05 16:16:30 -0500

    test: nixvim flake-parts outputs & loaders.

--- profiles/flakes/nixvim.nix
+++ profiles/flakes/nixvim.nix
@@ -1,4 +1,18 @@
-{inputs, ...}: {
+{inputs, ...}: let
+  #mkNixvim = inputs.nixvim.lib
+  inherit (inputs.haumea.lib) load loaders matchers transformers;
+  # Wrap profile "module" with enable option
+  #profile-wrapper = p: {inputs, config, lib, pkgs, ...}@args: let
+  #  cfg = config.profile-${path};
+  #  pcfg = (p args);
+  #in {
+  #  imports = pcfg.imports;
+  #  options.profile-${path} = pcfg.options // {
+  #    enable = lib.mkEnableOption "profile-${path}";
+  #  };
+  #  config = lib.mkIf cfg.enable (pcfg.config // builtins.removeAttrs pcfg ["imports" "options" "config"]);
+  #};
+in {
   perSystem = {
     config,
     pkgs,
@@ -12,10 +26,45 @@
       extraSpecialArgs = {inherit inputs;};
     };
   in {
-    checks.nixvim =
-      inputs.nixvim.lib.${pkgs.system}.check.mkTestDerivationFromNixvimModule nixvimModule;
-    packages.nvim =
-      inputs'.nixvim.legacyPackages.makeNixvimWithModule nixvimModule;
+    checks = {
+      #nixvim = inputs'.nixvim.lib.check.mkTestDerivationFromNixvimModule nixvimModule;
+      nixvim =
+        inputs.nixvim.lib.${system}.check.mkTestDerivationFromNixvimModule
+        nixvimModule;
+    };
+    packages = {
+      nvim = inputs'.nixvim.legacyPackages.makeNixvimWithModule nixvimModule;
+    };
   };
-  flake = {nixvimModules = {default = ../nixvim;};};
+
+  flake = {
+    #lib = {
+    #  mkNixvim = inputs'.nixvim.legacyPackages.makeNixvimWithModule nixvimModule;
+    #matchers.ignore-default = ext: f: {
+    #  matches = file: !(inputs.nixpkgs.lib.hasSuffix "default.${ext}") && inputs.nixpkgs.lib.hasSuffix ".${ext}" file && inputs.nixpkgs.lib.stringLength file > (inputs.nixpkgs.lib.stringLength ext + 1);
+    #  loader = f;
+    #};
+    #};
+    nixvimProfiles = {
+      default = ../nixvim;
+      languages = load {
+        src = ../nixvim/langs;
+        loader = loaders.verbatim;
+        #matcher = inputs.haumea.lib.matchers.nix;
+      };
+      plugins = load {
+        src = ../nixvim/plugins;
+        loader = loaders.verbatim;
+      };
+      colorschemes = load {
+        src = ../nixvim/colorschemes;
+        loader = loaders.verbatim;
+      };
+    };
+    nixvimModules = {
+      default = ../nixvim;
+      #profile-default = inputs.self.nixvimProfiles.default;
+      #builtins.attrNames inputs.self.nixvimProfiles
+    };
+  };
 }
