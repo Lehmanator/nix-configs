51ed01bb0c37e8a2dccdd5a7bd229794b98233bc
Sam Lehman <github@samlehman.dev>
2024-04-14 14:55:59 -0400

    fix: remove pkgs.gnomeExtensiosn.transparent-shell - removed from nixpkgs

--- nixos/profiles/desktop/de/gnome/extensions/appearance.nix
+++ nixos/profiles/desktop/de/gnome/extensions/appearance.nix
@@ -17,6 +17,5 @@
     material-you-color-theming # # Material You palette from wallpaper applied to adwaita (breaks light/dark theme)
     rounded-window-corners #     # Round window corners (fix non-adwaita app inconsistency) (11/23: GNOME45 incompat)
     snowy #                      # Snow effect on your desktop
-    transparent-shell #          # Transparent top bar, dash, workspace view
   ];
 }
