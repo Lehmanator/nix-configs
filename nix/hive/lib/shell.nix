6bb80187e3f74b465b445455470f3542ac4f1a70
Sam Lehman <github@samlehman.dev>
2024-07-22 07:34:16 -0400

    feat: more //hive/lib/strings

--- nix/hive/lib/shell.nix
+++ nix/hive/lib/shell.nix
@@ -4,13 +4,14 @@ let
   inherit (cell.lib.strings) fixedWidthStringAppend splitStringSized;
 in
 rec {
-  mkShellCategoryDivider = name: size: let
-    prefix = "# +---[";
-    suffix = let
-      chars-left = size - (lib.stringLength (prefix+name+2));
-    in "]${lib.strings.replicate "-" chars-left}+";
-  in "\n${prefix}${name}${suffix}";
-  mkShellSection = name: size: code: lib.concatLines [(mkShellCategoryDivider name size) code];
+  # mkShellCategoryDivider = name: size: let
+  #   prefix = "# +---[";
+  #   suffix = let
+  #     chars-left = size - (lib.stringLength (prefix+name+2));
+  #   in "]${lib.strings.replicate "-" chars-left}+";
+  # in "\n${prefix}${name}${suffix}";
+  # mkShellSection = name: size: code: lib.concatLines [(mkShellCategoryDivider name size) code];
+
   mkShellComment = line: "# ${line}";
   wrapLineBox = line: size: "|${fixedWidthStringAppend (size - 4) " " line}|";
   prefixLineComment = line: "# ${line}";
@@ -24,5 +25,27 @@ rec {
     strings = splitStringSized desc max_text_length;
   in map (s: mkShellSectionWrappedLine s size) strings;
 
+  # TODO: Allow specifying caps, filler, wrapper chars
+  # TODO: Better defaults, named sets of chars?
+  mkShellSectionDivider = name: size: {
+    caps ? ["+" "+"],
+    wrapper ? ["[" "]"],
+    filler ? "-",
+    filler-prefix ? "---"
+  }@c: let
+    wrapper = if (lib.stringLength > 0) && (name != " ") then [filler filler] else c.wrapper;
+    prefix = "${lib.elemAt c.caps 0}${c.filler-prefix}${lib.elemAt wrapper 0}";
+    known = "${prefix}${name}${lib.elemAt wrapper 1}";
+    size-rem = ((size - 2) - (lib.stringLength (lib.elemAt c.caps 1)));
+  in prefixLineComment (fixedWidthStringAppend size-rem c.filler known) + (lib.elemAt c.caps 1);
+
+  mkShellSectionBox = size: name: desc: { }@chars: lib.concatLines [
+    (mkShellSectionDivider name size {})  # Top w/ name
+    (mkShellSectionDescription desc size) # Desc lines
+    (mkShellSectionDivider "" size {})    # Bottom
+  ];
+
+  # TODO: mkShellSections
+  # TODO: mkShellSectionsDesc
   
 }
