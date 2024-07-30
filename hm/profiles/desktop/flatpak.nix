7c315d61a4a3e66804a1cdb23388591a7daa4e00
Sam Lehman <github@samlehman.dev>
2024-07-30 08:30:45 -0400

    feat: better flatpak shellAliases

--- hm/profiles/desktop/flatpak.nix
+++ hm/profiles/desktop/flatpak.nix
@@ -1,19 +1,25 @@
-{ self, inputs
-, config, lib, pkgs
-, options
-, flatpak-repos ? { flathub = "https://flathub.org/repo/flathub.flatpakrepo"; }
-, ...
-}:
-#lib.attrsets.recursiveUpdate
-{
-
+{ config, lib, pkgs, ... }: {
+  home.shellAliases = let
+    args = lib.cli.toGNUCommandLineShell {} {
+      assumeyes = true;
+      noninteractive = false;
+    };
+  in rec {
+    fp   = "flatpak";
+    fpb  = fp + " build";
+    fpo  = fp + " override";
+    fps  = fp + " search";
+    fpun = fp + " uninstall";
+    fpi  = fp + " install --or-update" + args;
+    fpup = fp + " update " + args;
+    fpu  = fpup;
+  };
 }
-#(lib.optionalAttrs (options?services.flatpak.packages) {
-#  services.flatpak.remotes = flatpak-repos;
-#  services.flatpak.preInitCommand = "";
-#  services.flatpak.postInitCommand = "";
-#
-#  # TODO: Add all default packages (runtimes, SDKs, themes, libs, plugins, codecs, etc.)
-#  services.flatpak.packages = [
-#  ];
+#(lib.optionalAttrs (lib.hasAttrByPath ["services" "flatpak" "packages"] options) {
+#  services.flatpak = {
+#    remotes = flatpak-repos;
+#    preInitCommand = "";
+#    postInitCommand = "";
+#    flatpak.packages = [ ]; # TODO: Add all default packages (runtimes, SDKs, themes, libs, plugins, codecs, etc.)
+#  };
 #})
--- nixos/profiles/desktop/flatpak.nix
+++ nixos/profiles/desktop/flatpak.nix
@@ -1,22 +1,16 @@
-{ inputs
-, config
-, lib
-, pkgs
-, user
+{ config, lib, pkgs, user, ... }: {
   #, flatpak-repos ? { flathub = "https://flathub.org/repo/flathub.flatpakrepo"; }
-, ...
-}:
-{
+
   # --- Package Info Integration ---
+  appstream.enable = true;
   services = {
     flatpak.enable = true;
     packagekit.enable = true;
   };
-  appstream.enable = true;
 
   # --- Fonts ---
-  fonts.fontconfig.enable = true;
-  fonts.fontDir.enable = true;
+  fonts = { fontconfig.enable = true; fontDir.enable = true; };
+
   # Rebuild font cache upon system activation
   #system.activationScripts.flatpakSystem.text = ''
   #  # TODO: Replace with primary user
@@ -27,15 +21,20 @@
 
   # --- Flatpak CLI ---
   users.users."${user}".extraGroups = [ "flatpak" ];
-  environment.shellAliases = {
+  environment.shellAliases = let
+    args = lib.cli.toGNUCommandLineShell {} {
+      assumeyes = true;
+      noninteractive = false;
+    };
+  in rec {
     fp = "flatpak";
-    fpb = "flatpak build";
-    fpi = "flatpak install";
-    fpo = "flatpak override";
-    fps = "flatpak search";
-    fpun = "flatpak uninstall";
-    fpup = "flatpak update";
-    fpu = "flatpak update";
+    fpb = fp + " build";
+    fpi = fp + " install --or-update" + args;
+    fpo = fp + " override";
+    fps = fp + " search";
+    fpun = fp + " uninstall";
+    fpup = fp + " update " + args;
+    fpu = fpup;
   };
 
   xdg.portal = {
@@ -44,23 +43,25 @@
   };
 }
 
-#(lib.optionalAttrs (options?services.flatpak.packages) {
+#(lib.optionalAttrs (lib.hasAttrByPath ["services" "flatpak" "packages"] options) {
 #  imports = [ ./flatpak-apps.nix ];
-#  services.flatpak.remotes = flatpak-repos;
-#  services.flatpak.preInitCommand = "";
-#  services.flatpak.postInitCommand = "";
-#  services.flatpak.packages = [
-#    "flathub:org.freedesktop.Sdk"
-#    "flathub:org.freedesktop.Platform"
-#    "flathub:org.freedesktop.Platform.ffmpeg-full"
-#    "flathub:org.freedesktop.Platform.html5-codecs"
-#    "flathub:org.freedesktop.Platform.openh264"
-#    "flathub:org.freedesktop.Platform.GL.default"     # TODO: Conditional if using OpenGL graphics
-#    "flathub:org.freedesktop.Platform.VAAPI.Intel"    # TODO: Conditional if using Intel graphics & VAAPI
-#    "flathub:org.freedesktop.LinuxAudio.Plugins.Calf"
-#    "flathub:org.freedesktop.LinuxAudio.Plugins.LSP"
-#    "flathub:org.freedesktop.LinuxAudio.Plugins.MDA"
-#    "flathub:org.freedesktop.LinuxAudio.Plugins.swh"
-#    "flathub:org.freedesktop.LinuxAudio.Plugins.ZamPlugins"
-#  ];
+#  services.flatpak = {
+#    remotes = flatpak-repos;
+#    preInitCommand  = "";
+#    postInitCommand = "";
+#    packages = [
+#      "flathub:org.freedesktop.Sdk"
+#      "flathub:org.freedesktop.Platform"
+#      "flathub:org.freedesktop.Platform.ffmpeg-full"
+#      "flathub:org.freedesktop.Platform.html5-codecs"
+#      "flathub:org.freedesktop.Platform.openh264"
+#      "flathub:org.freedesktop.Platform.GL.default"     # TODO: Conditional if using OpenGL graphics
+#      "flathub:org.freedesktop.Platform.VAAPI.Intel"    # TODO: Conditional if using Intel graphics & VAAPI
+#      "flathub:org.freedesktop.LinuxAudio.Plugins.Calf"
+#      "flathub:org.freedesktop.LinuxAudio.Plugins.LSP"
+#      "flathub:org.freedesktop.LinuxAudio.Plugins.MDA"
+#      "flathub:org.freedesktop.LinuxAudio.Plugins.swh"
+#      "flathub:org.freedesktop.LinuxAudio.Plugins.ZamPlugins"
+#    ];
+#  };
 #})
