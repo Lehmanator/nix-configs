cd3a9a13b42c9d48f03ab823f26ad923f839a818
Sam Lehman <github@samlehman.dev>
2024-08-03 01:48:07 -0400

    feat: update rust-motd config

--- nixos/profiles/motd.nix
+++ nixos/profiles/motd.nix
@@ -1,4 +1,8 @@
-{ config, lib, pkgs, user, ... }: {
+{ config, lib, pkgs, user, ... }:
+let
+  inherit (lib) mkDefault mkIf optional optionalAttrs unique;
+in
+{
   # --- Message of the Day (MotD) ---
   # Options:
   # - programs.rust-motd = { <rust-motd_config> };
@@ -15,41 +19,91 @@
   # - services.tox-node.motd = "<MOTD_TEXT>";
   # - services.xonotic.settings.sv_motd = "<MOTD_TEXT>";
   programs.rust-motd = {
-    enable = lib.mkDefault true;
-    enableMotdInSSHD = lib.mkDefault true;
-    order = [
-      "global"
-      "banner"
-      "uptime"
-      "last_login" # TODO: lib.mkBefore
-      "fail_2_ban"
-      "filesystems"
-      "memory"
-      #"ssl_certificates"
-      #"service_status"
-      #"user_service_status"
-      #"docker"
-      "weather"
-      "last_run" # # TODO: lib.mkAfter?
+    enable = mkDefault true;
+
+    # Let OpenSSH print MotD when entering new SSH session.
+    # Incompatible w/ opt: users.motd
+    enableMotdInSSHD = mkDefault true; 
+
+    order = [ "global" "banner" "weather" "uptime" "last_login" ] 
+      ++ (optional (config.services.fail2ban.enable) "fail2_ban")
+      ++ ["filesystems" "memory"]
+      #++ (with config.security.acme; optional (acceptTerms && (builtins.length (builtins.attrNames certs) > 0)) "s_s_l_certs" # old: "ssl_certificates"
+      ++ ["service_status" "user_service_status"]
+      #++ (with config.virtualisation; lib.optional (docker.enable || podman.enable) "docker")
+      ++ [ "last_run" 
     ];
-    refreshInterval = lib.mkDefault "*:0/5";
+    refreshInterval = mkDefault "*:0/5";
     settings = {
-      # Apply to all components
-      global = {
-        progress_full_character = "=";
-        progress_empty_character = "=";
-        progress_prefix = "[";
-        progress_suffix = "]";
-        progress_width = lib.mkDefault 40;
-        time_format = "%Y-%m-%d %H:%M:%S"; # TODO: relative time format?
+      # Applies to all components
+      global = let 
+        get-caps = n: if builtins.isString then {
+          progress_prefix = charsets.caps.${n};
+          progress_suffix = charsets.caps.${n};
+        } else {
+          progress_prefix = builtins.elemAt charsets.caps.${n} 0;
+          progress_suffix = builtins.elemAt charsets.caps.${n} 1;
+        };
+        mapChars = n: { 
+          progress_prefix          = builtins.elemAt charsets.${n} 0;
+          progress_full_character  = builtins.elemAt charsets.${n} 1;
+          progress_empty_character = builtins.elemAt charsets.${n} 2;
+          progress_suffix          = builtins.elemAt charsets.${n} 3;
+        };
+        show-seconds = false;
+        emptys = {
+          none = " ";
+          low = "░";
+          med = "▒";
+          dark = "▓";
+          full = "█";
+        };
+        charsets = {
+          caps = {
+            none = "";
+            space = " ";
+            thin  = ["▕" "▏"];
+            thick = ["┃" "┃"];
+            nobs  = ["─╢" "╟─"];
+            curve-up    = ["╰─╢" "╟─╯"];
+            curve-down  = ["╭─╢" "╟─╮"];
+            curve-2-down  = ["╭─┤" "├─╮"];
+            brackets = ["[" "]"];
+          };
+          equals = ["[" "=" "=" "]"];
+          block-thin-center = ["[" "■" " " "]"];
+          block-thin-top    = ["[" "▀" " " "]"];
+          block-thin-bottom = ["[" "▄" " " "]"];
+          block-thick-caps-none  = [""  "█" "░"  ""];
+          block-thick-caps-thin  = ["▕" "█" "░" "▏"];
+          block-thick-caps-thick = ["┃" "█" "░" "┃"];
+          block-thick-caps-nobs  = ["╢" "█" "░" "╟"];
+          # Shade Levels:  ░  ▒  ▓  █
+        };
+       in (mapChars "block-thin-center") // {
+        progress_width = mkDefault 40;
+        time_format = "%Y-%m-%d %H:%M" + (lib.optionalString show-seconds ":%S"); # TODO: relative time format?
       };
+      
+      # (if block then {
+      #   progress_full_character = "█";
+      #   progress_empty_character = "░";
+      #   progress_prefix = "┃";
+      #   progress_suffix = "┃"; # "╟";
+      # } else {
+      #   progress_full_character = "=";
+      #   progress_empty_character = "=";
+      #   progress_prefix = "[";
+      #   progress_suffix = "]";
+      # });
+      #■■■■■■■■■
 
       # Display singular banner
       # TODO: Set banner width to function of: `config.programs.rust-motd.settings.global.progress_width`
       # TODO: Set figlet style/font with `-d <font>`
       banner = {
-        color = lib.mkDefault "black"; # black|red|green|yellow|blue|magenta|cyan|white|light<Color>
-        command = lib.mkDefault "echo '${config.networking.hostName}' | ${pkgs.figlet}/bin/figlet -c"; # sh command to generate banner
+        color   = mkDefault "black"; # black|red|green|yellow|blue|magenta|cyan|white|light<Color>
+        command = mkDefault "echo '${config.networking.hostName}' | ${pkgs.figlet}/bin/figlet"; #-c";
       };
 
       # Display status of docker containers.
@@ -58,35 +112,22 @@
       #  "/nextcloud-nextcloud-1" = "Nextcloud";
       #};
 
-      # Display status of Fail2Ban jails
-      #fail_2_ban.jails = lib.mkIf config.services.fail2ban.enable ["sshd"];
-      fail_2_ban.jails = with config.services.fail2ban;
-        lib.mkIf enable (lib.attrsets.mapAttrsToList (n: v: n) jails);
-
       # Display filesystem space usage/consumption
+      # TODO: Auto list elements under /nix/persist
       filesystems = {
         root = "/";
         home = "/home";
-
-        # TODO: Auto list elements under /nix/persist
-        #nix-persist =
-        #  lib.mkIf (config.environment.persistence."/nix/persist")
-        #  "/nix/persist";
         nix-store = "/nix/store";
+        #nix-persist = mkIf (config.environment.persistence."/nix/persist") "/nix/persist";
       };
 
       # Display last login for users in attrset.
       # TODO: Possible to use `config.users.users.extraUsers`, etc.?
-      last_login = {
-        "${user}" = 5;
-        root = 5;
-      };
-
-      # Display time of last run of rust-motd
-      last_run = {};
+      last_login = { root=4; ${user}=4; };
+      last_run = {}; # Display time of last run of rust-motd
 
       # Display RAM & swap usage
-      memory.swap_pos = "beside"; # beside|below|none
+      memory.swap_pos = "below"; # beside|below|none
 
       # Display status of systemd system services
       # TODO: Move these to the NixOS profile that enables them.
@@ -99,44 +140,44 @@
       # - Figure out when services enabled by option not of form: `services.<name>`
       #   mkRenameServiceIfEnabled = s: n: if config.services.${s}.enable then { "${n}" = s; } else {};
       #   mkServiceRenames = lst: lib.foldr (item: acc: acc // (mkRenameServiceIfEnabled (builtins.tail item) (builtins.head item))) {} srv_list_system;
-      #service_status = {
-      #  Accounts = "accounts-daemon";
-      #  AppArmor = "apparmor";
-      #  Audit = "audit";
-      #  #auditd = "auditd";
-      #  Avahi = "avahi-daemon";
-      #  Bluetooth = "bluetooth";
-      #  D-Bus = "dbus";
-      #  Disks = "udisks2";
-      #  Containers = "containerd";
-      #  CRI-O = "crio";
-      #  Firewall = "nftables";
-      #  "Firmware Update" = "fwupd";
-      #  Geoclue = "geoclue";
-      #  LibVirt = "libvirtd";
-      #  LXD = "lxd";
-      #  "Network Manager" = "NetworkManager";
-      #  "Nix Daemon" = "nix-daemon";
-      #  OOM = "earlyoom"; # "systemd-oomd";
-      #  PolicyKit = "polkit";
-      #  Podman = "podman";
-      #  Printing = "cups";
-      #  "Printer Browser" = "cups-browsed";
-      #  SSH = lib.mkIf config.services.openssh.enable "sshd";
-      #  "SSH Guard" = "sshguard";
-      #  systemd-journald = "systemd-journald";
-      #  systemd-logind = "systemd-logind";
-      #  systemd-machined = "systemd-machined";
-      #  systemd-networkd = "systemd-networkd";
-      #  systemd-resolved = "systemd-resolved";
-      #  systemd-timesyncd = "systemd-timesyncd";
-      #  Tailscale = "tailscaled";
-      #  upower = "upower";
-      #  Waydroid = "waydroid-container";
-      #  Wireguard = "wg-netmanager";
-      #  "Wireguard SEA1" = "wireguard-wg-sea1";
-      #  wpa_supplicant = "wpa_supplicant";
-      #};
+      service_status = {
+        Accounts          = mkIf config.services.xserver.enable "accounts-daemon";
+        "D-Bus System"    = "dbus-broker";
+        Containers        = mkIf config.virtualisation.containerd.enable "containerd";
+        CRI-O             = mkIf config.virtualisation.cri-o.enable "crio";
+        Firewall          = "nftables";
+        "Network Manager" = mkIf config.networking.networkmanager.enable "NetworkManager";
+        "Nix Daemon"      = "nix-daemon";
+        PolicyKit         = "polkit";
+        Podman            = mkIf config.virtualisation.podman.enable "podman";
+        "SSH Daemon"      = mkIf config.services.openssh.enable "sshd";
+        "SSH Guard"       = mkIf config.services.sshguard.enable "sshguard";
+        systemd-logind    = "systemd-logind";
+        systemd-networkd  = "systemd-networkd";
+        Tailscale         = "tailscaled";
+        Waydroid          = mkIf config.virtualisation.waydroid.enable "waydroid-container";
+        "Wireguard SEA1"  = "wireguard-wg-sea1";
+        #  AppArmor = "apparmor";
+        #  Audit = "audit";
+        #  #auditd = "auditd";
+        #  Avahi = "avahi-daemon";
+        #  Bluetooth = "bluetooth";
+        #  Disks = "udisks2";
+        #  "Firmware Update" = "fwupd";
+        #  Geoclue = "geoclue";
+        #  LibVirt = "libvirtd";
+        #  LXD = "lxd";
+        #  OOM = "earlyoom"; # "systemd-oomd";
+        #  Printing = "cups";
+        #  "Printer Browser" = "cups-browsed";
+        #  systemd-journald = "systemd-journald";
+        #  systemd-machined = "systemd-machined";
+        #  systemd-resolved = "systemd-resolved";
+        #  systemd-timesyncd = "systemd-timesyncd";
+        #  upower = "upower";
+        #  Wireguard = "wg-netmanager";
+        #  wpa_supplicant = "wpa_supplicant";
+      };
 
       # Display SSL certs & renewal times
       #ssl_certificates = {
@@ -148,29 +189,38 @@
       #};
 
       # Display system uptime
-      uptime = {prefix = "Uptime: ";};
+      uptime.prefix = "Uptime: ";
 
       # Display status of systemd user services
-      #user_service_status = {
-      #  "D-Bus" = "dbus";
-      #  DConf = "dconf";
-      #  "GPG Agent" = "gpg-agent";
-      #  GVFS = "gvfs-daemon";
-      #  Keyring =
-      #    lib.mkIf config.services.gnome.gnome-keyring.enable "gnome-keyring";
-      #  MPD = "mdp";
-      #  Portals = "flatpak-portal";
-      #};
+      user_service_status = {
+        "D-Bus Session" = "dbus";
+        "GPG Agent"     = "gpg-agent";
+        GVFS            = "gvfs-daemon";
+        # DConf = "dconf";
+        # Keyring = lib.mkIf config.services.gnome.gnome-keyring.enable "gnome-keyring";
+        # MPD = "mdp";
+        # Portals = "flatpak-portal";
+      };
 
       # Display weather from wttr.in
-      weather = {
-        loc = "Erie,Pennsylvania";
-        style = "day"; # oneline|day|full
+      # https://github.com/chubin/wttr.in
+      weather = let
+        glyph-type = "emoji"; # emoji | nerdfont | unicode | ascii
+        extra-data = true;
+        format = "v2"; #
+      in rec {
+        # loc = "Erie";  #"Erie,Pennsylvania";
+        # style = "day"; # oneline|day|full
         timeout = 5;
-        #url = "https://wttr.in/Erie,Pennsylvania?format=4";
+        url = "https://en.wttr.in/Erie?format=3";
+        # url = "https://en.wttr.in/Erie?u&format=3";
         #user_agent = "Mozilla/5.0 ()";
         #proxy = "http://proxy:8080";
       };
+    } // optionalAttrs config.services.fail2ban.enable {
+      # Display status of Fail2Ban jails
+      #fail2_ban.jails = lib.mkIf config.services.fail2ban.enable ["sshd"];
+      fail_2_ban.jails = unique (["sshd"]++(builtins.attrNames config.services.fail2ban.jails));
     };
   };
 }
