541678a81db2d7989954ddc2f2b1c28ba2bc1fe5
Sam Lehman <github@samlehman.dev>
2024-08-03 01:44:07 -0400

    fix: auto-add primary user to sshd group

--- nixos/profiles/users/primary.nix
+++ nixos/profiles/users/primary.nix
@@ -1,16 +1,12 @@
-{ config
-, lib
-, pkgs
-, user
-, ...
-}:
+{ config, lib, pkgs, user, ... }:
 {
-  imports = [ ];
-
   # TODO: Inherit from default
   users.users."${user}" = {
     isNormalUser = true;
+
+    # TODO: Migrate to more standard "username:username"
+    # TODO: Create new "users" group & add primary user.
     group = "users";
-    extraGroups = [ "dialout" "wheel" ];
+    extraGroups = [ "dialout" "wheel" "sshd" ];
   };
 }
