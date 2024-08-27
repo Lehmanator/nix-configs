247e17506dde52db8a1ff75cc8fbcd70fa6278a2
Sam Lehman <github@samlehman.dev>
2024-08-27 13:19:49 -0400

    fix: add Valent flatpak remote

--- nixos/profiles/desktop/de/gnome/extensions/gsconnect.nix
+++ nixos/profiles/desktop/de/gnome/extensions/gsconnect.nix
@@ -1,18 +1,11 @@
-{ self
-, inputs
-, config
-, lib
-, pkgs
-, ...
-}:
+{ config, lib, pkgs, ... }:
 let
   implementation = "valent";
+  prefer-flatpak = false;
 in
 {
-  imports = [
-  ];
-
   # Allow GSConnect thru firewall
+  # TODO: Only allow thru wireguard tunnels
   networking.firewall = {
     enable = true;
     allowedTCPPortRanges = [{ from = 1714; to = 1764; }];
@@ -28,11 +21,17 @@ in
   #];
 
   # Install implementation of GSConnect & shell-extension
-  environment.systemPackages =
-    if (implementation == "valent") then [
-      pkgs.valent
-      #pkgs.gnomeExtensions.valent
-    ] else [
-      pkgs.gnomeExtensions.gsconnect
-    ];
+  environment.systemPackages = if (implementation == "valent")
+     then [ pkgs.valent ]  # pkgs.gnomeExtensions.valent
+     else [ pkgs.gnomeExtensions.gsconnect ];
+
+  services.flatpak = lib.mkIf prefer-flatpak {
+    remotes  = [{ name="valent"; location = "https://valent.andyholmes.ca/repo"; }];
+    packages = []
+      ++ lib.optional (implementation=="valent")     [{ origin="valent";  appId="ca.andyholmes.Valent"; }]
+      ++ lib.optional (implementation=="kdeconnect") [{ origin="flathub"; appId=""; }]
+    ;
+  };
+
+  # TODO: DConf settings for GSConnect / Valent GNOME extensions
 }
