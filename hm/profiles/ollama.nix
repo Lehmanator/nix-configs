dc8bc81008a25846447d93188c621463a4a3e237
Sam Lehman <github@samlehman.dev>
2024-10-01 02:53:59 -0400

    feat: homeProfiles.ollama - add Alpaca flatpak

--- hm/profiles/ollama.nix
+++ hm/profiles/ollama.nix
@@ -1,10 +1,9 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{ config, pkgs, lib, ... }: {
   home = {
     sessionVariables.OLLAMA_HOST = "45.42.244.197:11434";
     packages = [pkgs.ollama];
   };
+  services.flatpak.packages = [
+  ] ++ lib.mkIf config.programs.gnome-shell.enable "com.jeffser.Alpaca"
+  ;
 }
