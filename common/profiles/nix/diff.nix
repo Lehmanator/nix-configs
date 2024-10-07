0ef24543a7a7939b42827d51b5d1767b73098b3e
Sam Lehman <github@samlehman.dev>
2024-10-07 14:01:59 -0400

    refactor: commonProfiles.nix.diff - cleanup

--- common/profiles/nix/diff.nix
+++ common/profiles/nix/diff.nix
@@ -1,41 +1,34 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: let
+{ inputs, config, lib, pkgs, ... }@moduleArgs: 
+let
+  inherit (lib) getExe;
+
+  nix    = getExe config.nix.package;
+  rg     = getExe pkgs.ripgrep;
+  awk    = getExe pkgs.gawk;
+  sed    = getExe pkgs.gnused;
+  nvd    = getExe pkgs.nvd;
+  choose = getExe pkgs.choose;
+
   # TODO: Wrap all diff commands in pretty tables with `column` command.
   # TODO: Enforce all columns same width.
-  #
-  # --- Diff Hook --------------------------------
-  # Runs on every activation/build?
-  # TODO: Fix build failure to encode character \9472
-  #diffHook = pkgs.writeShellApplication {
-  #  name = "pretty-nix-diff-hook";
-  #  runtimeInputs = [];
-  #  text = mkWrapper "nix-diff" "${
-  #    lib.getExe pkgs.nix-diff
-  #  } --color always --skip-already-compared --word-oriented --environment --squash-text-diff";
-  #};
-  #
+  
   # Wrap commands in pretty box using unicode box chars.
-  mkWrapper = import ../../../lib/shell/wrap-command-box.nix {inherit pkgs;};
+  mkWrapper = import "${inputs.self}/lib/shell/wrap-command-box.nix" moduleArgs;
 in {
   system.activationScripts = {
     diff-closures = {
       supportsDryActivation = true;
       text = mkWrapper "nix store diff-closures" ''
-        ${
-          lib.getExe config.nix.package
-        } store diff-closures /run/current-system "$systemConfig" | ${
-          lib.getExe pkgs.ripgrep
-        } --color=always -w "→" | ${
-          lib.getExe pkgs.ripgrep
-        } --color=always -w "KiB" | column --table --separator " ,:" | "${pkgs.choose}/bin/choose" 0:1 -4:-1 | ${
-          lib.getExe pkgs.gawk
-        } '{s=$0; gsub(/\0 33\[[ -?]*[@-~]/,"",s); print s "\t" $0}' | sort -k5,5gr | "${pkgs.choose}/bin/choose" 6:-1 | column --table | ${
-          lib.getExe pkgs.gnused
-        } 's/^/│ /'
+        ${nix} store diff-closures /run/current-system "$systemConfig" | \
+        ${rg} --color=always -w "→" | \
+        ${rg} --color=always -w "KiB" | \
+        column --table --separator " ,:" | \
+        ${choose} 0:1 -4:-1 | \
+        ${awk} '{s=$0; gsub(/\0 33\[[ -?]*[@-~]/,"",s); print s "\t" $0}' | \
+        sort -k5,5gr | \
+        ${choose} 6:-1 | \
+        column --table | \
+        ${sed} 's/^/│ /'
       '';
     };
 
@@ -43,20 +36,15 @@ in {
     diff-versions = {
       supportsDryActivation = true;
       text = mkWrapper "nvd diff" ''
-        ${
-          lib.getExe pkgs.nvd
-        } --color always --nix-bin-dir ${config.nix.package}/bin diff /run/current-system "$systemConfig" | ${
-          lib.getExe pkgs.gnused
-        } 's/^/│ /'
+        ${nvd} --color always --nix-bin-dir ${config.nix.package}/bin diff /run/current-system "$systemConfig" | \
+        ${sed} 's/^/│ /'
       '';
     };
 
     #diff-derivations = {
     #  supportsDryActivation = true;
     #  text = mkWrapper "nix-diff" ''
-    #    ${
-    #      lib.getExe pkgs.nix-diff
-    #    } /run/current-system "$systemConfig" --skip-already-compared --word-oriented --squash-text-diff --color always
+    #    ${lib.getExe pkgs.nix-diff} /run/current-system "$systemConfig" --skip-already-compared --word-oriented --squash-text-diff --color always
     #  '';
     #};
   };
@@ -76,5 +64,16 @@ in {
   #  #'';
   #};
 
-  #environment.systemPackages = [diffHook];
+  #environment.systemPackages = [
+  # --- Diff Hook --------------------------------
+  # Runs on every activation/build?
+  # TODO: Fix build failure to encode character \9472
+  #(pkgs.writeShellApplication {
+  #  name = "pretty-nix-diff-hook";
+  #  runtimeInputs = [];
+  #  text = mkWrapper "nix-diff" "${
+  #    lib.getExe pkgs.nix-diff
+  #  } --color always --skip-already-compared --word-oriented --environment --squash-text-diff";
+  #})
+  #];
 }
