a6c8836709d9430997d2eb6c8e4c40ad933b14b4
Sam Lehman <github@samlehman.dev>
2024-10-07 13:11:17 -0400

    feat: homeProfiles.home-manager - enable manuals

--- hm/profiles/home-manager.nix
+++ hm/profiles/home-manager.nix
@@ -5,12 +5,26 @@
 
   # home-manager manuals
   manual = {
-    html.enable     = true;  # $ home-manager-help
-    json.enable     = true;  # $ jq '.' $profileDir/share/doc/home-manager/options.json
-    manpages.enable = true;  # $ man home-configuration.nix
+    # TODO: Create wrapper script for `home-manager-help` to:
+    #       - navigate to options page
+    #       - open to specific option
+    # $ home-manager-help
+    html.enable     = true;  
+
+    # TODO: Create wrapper scripts for `options.json` to:
+    #       - view with jq
+    #       - view / search interactively
+    # $ jq '.' $profileDir/share/doc/home-manager/options.json
+    json.enable     = true;
+
+    # $ man home-configuration.nix
+    manpages.enable = true;
   };
 
+  # Allow home-manager to install & manage itself
+  # TODO: Figure out if this has any effect when managed by NixOS
   programs.home-manager.enable = true;
+  # TODO: Figure out if this has any effect when managed by NixOS
   services.home-manager.autoUpgrade = {
     enable = true;
     frequency = "weekly";
