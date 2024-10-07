5581ee7cd3d484e8640c6fa9c50293d8c6aa8c10
Sam Lehman <github@samlehman.dev>
2024-10-07 13:30:48 -0400

    refactor: homeProfiles.eza - cleanup

--- hm/profiles/eza.nix
+++ hm/profiles/eza.nix
@@ -3,38 +3,41 @@
 # - Use option values to set config for all ls programs. `programs.ls = {}`
 # - Use `programs.ls.alias="eza"` to set `programs.eza.enableAliases=true`
 let
+  inherit (lib) optional;
   sort = {
-    dirs = "first";
-    field = "name";
+    dirs    = "first";
+    field   = "name";
     reverse = false;
     ignored = true && show.git;
   };
   view = {
-    layout = "grid"; # grid | tree | oneline
+    layout  = "grid"; # grid | tree | oneline
     recurse = true;
-    depth = 3;
+    depth   = 3;
   };
   show = {
-    classify = false;
-    color = true;
-    git = true;
-    icons = true;
-    headers = true;
-    links = true;
-    symlinks = true;
-    total = true;
+    classify  = false;
+    color     = true;
+    git       = true;
+    icons     = true;
+    headers   = true;
+    links     = true;
+    symlinks  = true;
+    total     = true;
     hyperlink = true;
   };
 in {
   programs.eza = {
     enable = true;
     inherit (show) git icons;
+
+    # TODO: Use lib.cli.toGNUCommandLine {} attrs;
     extraOptions =
       ["--level=${builtins.toString view.depth}"]
-      ++ lib.optional show.hyperlink "--hyperlink"
-      ++ lib.optional show.headers "--header"
-      ++ lib.optional sort.ignored "--git-ignore"
-      ++ lib.optional (sort.dirs == "first") "--group-directories-first"
+      ++ optional show.hyperlink "--hyperlink"
+      ++ optional show.headers   "--header"
+      ++ optional sort.ignored   "--git-ignore"
+      ++ optional (sort.dirs=="first") "--group-directories-first"
       #++ lib.optional classify "--classify"
       #++ lib.optional color "--color-scale=gradient"
       ;
