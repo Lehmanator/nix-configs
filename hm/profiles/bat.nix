4dfb532a6d95ac35fac0635c75ce8d1a7d27038a
Sam Lehman <github@samlehman.dev>
2024-10-07 14:03:20 -0400

    fix: homeProfiles.bat - disable broken shellAliases [WIP]

--- hm/profiles/bat.nix
+++ hm/profiles/bat.nix
@@ -25,7 +25,8 @@
   programs.bat = {
     enable = true;
 
-    extraPackages = with pkgs.bat-extras; [batdiff batgrep batman batpipe batwatch prettybat];
+    # extraPackages = with pkgs.bat-extras; [batdiff batgrep batman batpipe batwatch prettybat];
+    extraPackages = builtins.attrValues (lib.filterAttrs (_: v: builtins.isAttrs v) pkgs.bat-extras);
 
     # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.bat.syntaxes
     # syntaxes = {};
@@ -61,7 +62,7 @@
   };
 
   programs.zsh.shellGlobalAliases = {
-    BAT    = "| bat";
+    BAT    = "| bat";      # TODO: set previous cmd stdin as title
     BCAT   = "| bat";
     BDIFF  = "| batdiff";
     BGREP  = "| batgrep";
@@ -78,8 +79,10 @@
 
       # Use bat as pager for manpages
       # NOTE: Doesnt work with Mandocs `man` impl
-      MANPAGER  = lib.mkIf config.programs.bat.enable "sh -c 'col -bx | bat -l man -p'";
-      MANROFFOPT= lib.mkIf config.programs.bat.enable "-c";  # Fixes formatting
+      MANPAGER   = lib.mkIf config.programs.bat.enable
+        "sh -c 'col -bx | bat -l man -p'";
+      MANROFFOPT = lib.mkIf config.programs.bat.enable
+        "-c";  # Fixes formatting
 
       # PAGER     = "";
       # LESS      = "";
@@ -90,32 +93,75 @@
     };
 
     # TODO: Theme previewer (w/ file) = "bat --list-themes | fzf --preview='bat --theme={} --color=always flake.nix'";
-    shellAliases = {
-      b    = "bat";
-      bcat = "bat";
-      cab  = "bat";
+    shellAliases = 
+    # let
+    #   getBaseCmd = c: lib.removePrefix "bat" (lib.removeSuffix "bat" c); 
+    #   batAliases = lib.concatMapAttrs (n: v: 
+    #   let
+    #     base = getBaseCmd n;
+    #     pre = "b" + builtins.substring 1 99 base;
+    #     suf = builtins.
+    #     cmd = n;
+    #     # cmd = lib.getExe v + " " + lib.concatStringsSep " " args;
+    #   in rec {
+    #     "${base}" = cmd;
+    #     "b${base}" = cmd;
+    #     "${base}b" = cmd;
+    #   }) config.programs.bat.extraPackages;
 
-      bdiff = "batdiff";
-      difb  = "batdiff";
+    #   mkBatAliases = cmd: aliases: let
+    #     # args = builtins.tail (lib.splitString " " cmd);
+    #     # alias = builtins.concatStringsSep " " [(lib.getExe pkgs.bat-extras.${prog})] ++ (builtins.tail (lib.splitString " " cmd));
+    #     # args = builtins.concatStringsSep " " builtins.tail (lib.splitString " " [lib.getExe pkgs.bat-extras.${prog}] ++ cmd);
+    #     # alias = lib.getExe pkgs.bat-extras.${prog} + 
+    #     prog = builtins.head (lib.splitString " " cmd);
+    #   in lib.optionalAttrs
+    #     # Alias creation condition: pname in bat.extraPackages
+    #     (builtins.elem config.programs.bat.extraPackages pkgs.bat-extras.${prog})
 
-      bgrep = "batgrep";
-      greb  = "batgrep";
+    #     # Create attr for each alias set to executable of ${prog}
+    #     (lib.genAttrs aliases (_:                      # forAll {<alias..>=cmd}
+    #       builtins.concatStringsSep " "                # Assemble command
+    #         [(lib.getExe pkgs.bat-extras.${prog})] ++  # Prefix command w/ bin
+    #           builtins.tail (lib.splitString " " cmd)  # Reconnect args string
+    #     ))
+    #   ;
+    #   mkBatExtra = util: mkBatAliases ["b${util}" "${util}b" "${builtins.substring 0 3 util}b"];
+    #   # batExtras = lib.concatMapAttrs
+    #   batExtras = args: lib.concatMapAttrs (pname: v:
+    #   let
+    #     prog = builtins.substring 3 99 pname;
+    #     cmd  = builtins.concatStringsSep " " ([(lib.getExe v)]) ++ args;
+    #   in {
+    #     "b${prog}"  = cmd;
+    #      "${prog}b" = cmd;
+    #      "${builtins.substring 0 3 prog}b" = cmd;
+    #   }) config.programs.bat.extraPackages;
+    # in 
+    # (mkBatAliases "bat"      ["bcat"  "catb"  "cab" "b"])
+    # (mkBatAliases "batdiff"  ["bdiff" "diffb" "difb"])
+    # (mkBatAliases "batgrep"  ["bgrep" "grepb" "greb"])
+    # (mkBatAliases "batman"   ["bman"  "manb"  "mab" ])
+    # (mkBatAliases "batwatch" ["bman"  "manb"  "mab" ])
 
-      bman  = "batman";
-      mab   = "batman";
-
-      btail = "tail | bat --paging=never --style=plain --color=always --language log";
-      taib  = "tail | bat --paging=never --style=plain --color=always --language log";
-
-      bwatch = "batwatch";
-      watcb  = "batwatch";
-
-      # TODO: lib.getFuzzyPreviewer "text"
-      batfzfpreview = lib.mkIf config.programs.fzf.enable
-        "bat --color=always --style=numbers --line-range=:5000";
-
-      bathelp = "bat --language help --color=always --style=plain";
-      belp    = "bat --language help --color=always --style=plain";
+    rec {
+      bcat ="bat";      catb="bat";      cab="bat";     b="bat";
     };
+    #   bdiff="batdiff"; diffb="batdiff"; difb="batdiff"; d="batdiff";
+    #   bgrep="batgrep"; grepb="batgrep"; greb="batgrep"; #rg="batgrep";
+    #   bman ="batman";   manb="batman";   mab="batman";  m="batman";
+
+    #   btail = "tail | bat --paging=never --style=plain --color=always --language log"; tailb=btail; taib=btail;
+
+    #   watch="batwatch"; bwatch=watch; watchb=watch; watcb=watch;
+    #   cat="bat"; bcat="bat"; catb="bat"; cab="cat"; b="bat"; 
+
+    #   # TODO: lib.getFuzzyPreviewer "text"
+    #   batfzfpreview = lib.mkIf config.programs.fzf.enable
+    #     "bat --color=always --style=numbers --line-range=:5000";
+
+    #   bathelp = "bat --language help --color=always --style=plain";
+    #   belp    = "bat --language help --color=always --style=plain";
+    # } // lib.optionalAttrs (builtins.elem config.programs.bat.extraPackages pkgs.batdiff) ;
   };
 }
