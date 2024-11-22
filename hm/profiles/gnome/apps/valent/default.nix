e36d84cc30d097e81be2d540cd8434b4c6bf67fa
Sam Lehman <github@samlehman.dev>
2024-11-22 03:07:17 -0500

    feat: pkgs.valent - update

--- hm/profiles/gnome/apps/valent/default.nix
+++ hm/profiles/gnome/apps/valent/default.nix
@@ -1,12 +1,42 @@
-{ lib, pkgs, ... }: 
+{ inputs, lib, pkgs, ... }: 
 let
   prefer-flatpak = false;
+
+  unstable = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system};
+
+  walbottle = pkgs.callPackage (inputs.self + "/nixos/packages/walbottle.nix") {};
+  valent-app = unstable.valent.overrideAttrs (p: {
+    version = "1.0.0.alpha.47-unstable-2024-11-20";
+    src = pkgs.fetchFromGitHub {
+      owner = "andyholmes";
+      repo = "valent";
+      rev = "0ea090301e6095e4d2141e557b32a68e03f3452c";
+      hash = "sha256-quSa3rXtRNvig31dfurbDllBDG4h09wRk9dhWEBLc+s=";
+      fetchSubmodules = true;
+    };
+    # TODO: Figure out how to re-enable pulseaudio plugin
+    mesonFlags = p.mesonFlags ++ [(lib.mesonBool "plugin_pulseaudio" false)];
+    nativeBuildInputs = p.nativeBuildInputs ++ [pkgs.libsysprof-capture walbottle];
+    buildInputs = p.buildInputs ++ [pkgs.appstream pkgs.cmake]; #[pkgs.libcanberra pkgs.gsound pkgs.pulseaudioFull pkgs.libpulseaudio];
+  });
+
+  valent-ext = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.gnomeExtensions.valent.overrideAttrs (p: {
+    version = "1.0.0.alpha.47-unstable-2024-11-20";
+    src = pkgs.fetchFromGitHub {
+      owner = "andyholmes";
+      repo = "gnome-shell-extension-valent";
+      rev = "99ff268a8fc64416c62512efb4710410ea1ee4b5";
+      hash = "sha256-jmafFcoTBav9gV3MCPh1nJkCoIlCAwJ8GqOmRyFElyk=";
+      fetchSubmodules = true;
+    };
+  });
+
 in
 {
   services.flatpak = lib.mkIf prefer-flatpak {
     remotes = [{ name = "Valent"; location = "https://valent.andyholmes.ca/repo"; }];
     packages = [{ origin="Valent"; appId="ca.andyholmes.Valent"; }];
   };
-  home.packages = lib.mkIf (! prefer-flatpak) [ pkgs.valent ];
-  programs.gnome-shell.extensions = [{ package = pkgs.gnomeExtensions.valent; }];
+  home.packages = lib.mkIf (! prefer-flatpak) [ valent-app ];
+  programs.gnome-shell.extensions = [{ package = valent-ext; }];
 }
--- nixos/profiles/desktop/gnome/extensions/gsconnect.nix
+++ nixos/profiles/desktop/gnome/extensions/gsconnect.nix
@@ -1,29 +1,66 @@
-{ config, lib, pkgs, ... }:
+{ inputs, config, lib, pkgs, ... }:
 let
   implementation = "valent";
   prefer-flatpak = false;
+
+  unstable = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system};
+
+  walbottle = pkgs.callPackage (inputs.self + "/nixos/packages/walbottle.nix") {};
+
+  valent-app = unstable.valent.overrideAttrs (p: {
+    version = "1.0.0.alpha.47-unstable-2024-11-20";
+    src = pkgs.fetchFromGitHub {
+      owner = "andyholmes";
+      repo = "valent";
+      rev = "0ea090301e6095e4d2141e557b32a68e03f3452c";
+      hash = "sha256-quSa3rXtRNvig31dfurbDllBDG4h09wRk9dhWEBLc+s=";
+      fetchSubmodules = true;
+    };
+
+    # TODO: Figure out how to re-enable pulseaudio plugin
+    mesonFlags = p.mesonFlags ++ [(lib.mesonBool "plugin_pulseaudio" false)];
+    nativeBuildInputs = p.nativeBuildInputs ++ [pkgs.libsysprof-capture walbottle];
+    buildInputs = p.buildInputs ++ [pkgs.appstream pkgs.cmake]; #[pkgs.libcanberra pkgs.gsound pkgs.pulseaudioFull pkgs.libpulseaudio];
+    # passthru.updateScript = { };
+  });
+
+  valent-ext = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.gnomeExtensions.valent.overrideAttrs (p: {
+    version = "1.0.0.alpha.47-unstable-2024-11-20";
+    src = pkgs.fetchFromGitHub {
+      owner = "andyholmes";
+      repo = "gnome-shell-extension-valent";
+      rev = "99ff268a8fc64416c62512efb4710410ea1ee4b5";
+      hash = "sha256-jmafFcoTBav9gV3MCPh1nJkCoIlCAwJ8GqOmRyFElyk=";
+      fetchSubmodules = true;
+    };
+    # passthru.updateScript = { # };
+  });
+
+  extension-package = if implementation=="valent"
+    then valent-ext else pkgs.gnomeExtensions.gsconnect;
 in
 {
+
+  programs.kdeconnect = {
+    enable = true;  
+    package = valent-app;
+  };
+  
   # Allow GSConnect thru firewall
   # TODO: Only allow thru wireguard tunnels
   networking.firewall = {
-    enable = true;
     allowedTCPPortRanges = [{ from = 1714; to = 1764; }];
     allowedUDPPortRanges = [{ from = 1714; to = 1764; }];
   };
 
   # Enable Firefox integration
-  #programs.firefox.nativeMessagingHosts.gsconnect = true;
-  #programs.firefox.nativeMessagingHosts.packages = [
-  #  pkgs.gnomeExtensions.gsconnect
-  #  pkgs.gnomeExtensions.valent
-  #  pkgs.valent
-  #];
+  programs.firefox.nativeMessagingHosts = {
+    gsconnect = true;
+    packages = [config.programs.kdeconnect.package extension-package];
+  };
 
   # Install implementation of GSConnect & shell-extension
-  environment.systemPackages = if (implementation == "valent")
-     then [ pkgs.valent ]  # pkgs.gnomeExtensions.valent
-     else [ pkgs.gnomeExtensions.gsconnect ];
+  environment.systemPackages = [config.programs.kdeconnect.package extension-package];
 
   services.flatpak = lib.mkIf prefer-flatpak {
     remotes  = [{ name="valent"; location = "https://valent.andyholmes.ca/repo"; }];
