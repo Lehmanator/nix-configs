4035bcc0ea1a8da811ba446f72990a288137092e
Sam Lehman <github@samlehman.dev>
2024-12-09 14:34:48 -0500

    feat: homeProfiles.languages.markdown - extra utils

--- hm/profiles/languages/markdown.nix
+++ hm/profiles/languages/markdown.nix
@@ -9,6 +9,7 @@
       pkgs.markdown-oxide
       pkgs.marksman
       pkgs.mdformat
+      pkgs.mdctags
     ];
     languages = {
       language-server = {
@@ -35,6 +36,7 @@
 
   # --- VSCode ---
   # https://marketplace.visualstudio.com/items?itemName=FelixZeller.markdown-oxide
+  # vscode-extensions.shd101wyy.markdown-preview-enhanced
 
   # --- Zed ------
   programs.zed-editor = {
@@ -42,4 +44,20 @@
     # extraPackages = [ pkgs.markdown-oxide ];
     extensions = ["markdown-oxide"];
   };
+
+  home.packages = [
+    pkgs.mdcat     # cat for Markdown
+    pkgs.mdctags   # Markdown file tags
+    pkgs.md-tui    # Markdown TUI renderer
+    pkgs.mdr       # Markdown Renderer
+    pkgs.mdsh      # Markdown shell pre-processor
+    pkgs.papeer    # Convert websites into ebooks & Markdown
+    pkgs.mdbook    # Create books from Markdown.
+    pkgs.md2pdf    # Markdown to PDF conversion tool
+    pkgs.glow      # Fancy Markdown renderer CLI
+    pkgs.ronn      # Markdown-based tool for building manpages
+    pkgs.marked-man # Markdown to roff
+    pkgs.mandown    # Markdown to groff (manpage) converter
+    pkgs.comrak     # Markdown formatter & HTML converter
+  ];
 }
