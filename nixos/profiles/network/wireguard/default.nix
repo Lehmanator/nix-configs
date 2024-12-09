ff33696de4506c8887b3866e33b656fdd3cc508d
Sam Lehman <github@samlehman.dev>
2024-12-09 15:38:37 -0500

    feat: nixosProfiles.network.wireguard - enable wgautomesh, config changes [WIP]

--- nixos/profiles/network/wireguard/default.nix
+++ nixos/profiles/network/wireguard/default.nix
@@ -1,8 +1,5 @@
 { inputs
-, config
-, lib
-, pkgs
-, user
+, config, lib, pkgs
 , ...
 }:
 # See: https://nixos.wiki/wiki/WireGuard
@@ -17,7 +14,6 @@
 # - ...
 {
   imports = [
-    #inputs.agenix.nixosModules.age
     #inputs.sops-nix.nixosModules.sops
     #./backends/generic.nix
     #./backends/networkmanager.nix
@@ -26,47 +22,67 @@
     #./peers/mullvad-ashburn-adblock.nix
     #./peers/sea1.nix
     ./sea1.nix
-    #./services/wgautomesh.nix
-    #./services/wg-netmanager.nix
+    ./wgautomesh.nix
+    #./wg-netmanager.nix
   ];
 
   # Fixes IPv6-based exit node servers
   # TODO: Move to server config
+  # TODO: Make lan-home wireguard interface
   networking.nftables.enable = true;
   networking.wireguard = {
     enable = true;
-    #interfaces = {
-    #  wg0 = {
-    #    allowedIPsAsRoutes = true; # default: true
-    #    generatePrivateKeyFile = true; # default: false
-    #    privateKeyFile = "/etc/wireguard/wg0.privkey";
-    #    ips = [ "192.168.125.1/24" ];
-    #    #listenPort = 42270;              # default: null  (51820)
-    #    #mtu = 1420;                      # default: null
-    #    #interfaceNamespace = null;      # default: null
-    #    #peers = [{
-    #    #  allowedIps = [ "<LocalIP>/32" ];
-    #    #  dynamicEndpointRefreshRestartSeconds = 5;  # default: null
-    #    #  dynamicEndpointRefreshSeconds = 0;         # default: 0
-    #    #  endpoint = "<RemoteIP|Hostname>:<Port>";
-    #    #  persistentKeepalive = 25;                  # default: null
-    #    #  presharedKey = null;                       # default: null
-    #    #  presharedKeyFile = "/run/secrets/wireguard-${config.networking.wireguard.interfaces[0]}";                   # default: null
-    #    #  publicKey = "";
-    #    #}];
-    #    #postSetup = "";
-    #    #postShutdown = "";
-    #    #preSetup = "";
-    #    #privateKey = "";
-    #    #privateKeyFile = "${host.dirs.secrets}/wireguard-${config.networking.wireguard.interfaces[0]}.privkey";
-    #    #socketNamespace = null; # default: null
-    #    #table = "main";         # default: "main"
-    #  };
-    #};
+    interfaces.wg0 = {
+      privateKeyFile = config.sops.secrets.wireguard-wg0-privkey.path;
+      listenPort = 51820;
+      ips = [
+        "10.0.1.0/24"
+        "fd42:42:42::1/64"
+      ];
+      peers = [
+        # { endpoint = "45.42.244.130:51420";
+        #   allowedIPs = ["45.42.244.62/32" "2602:fcc3::c00b/128"];
+        #   persistentKeepalive = 10;
+        #   publicKey = "/XgG41yW5zjQvLJl3D6LKVx5UGMb5vmvQc5GHeW/oFc=";
+        # }
+        { # --- Router ---
+          endpoint = "192.168.1.1:51820";  # TODO: External IP addresses
+          allowedIPs = ["192.168.0.0/16"]; # TODO: External IP addresses
+          publicKey = "j9oRo3iHKygIKuQc0dF6OOkyhA8M2S9bOY6f7C4PgxE=";
+        }
+        # { # --- Wyse ---
+        #   endpoint = "wyse:51820";
+        #   allowedIps = ["192.168.1.3/32"];
+        # }
+        # { allowedIps = [ "<LocalIP>/32" ];
+        #   dynamicEndpointRefreshRestartSeconds = 5;  # default: null
+        #   dynamicEndpointRefreshSeconds = 0;         # default: 0
+        #   endpoint = "<RemoteIP|Hostname>:<Port>";
+        #   persistentKeepalive = 25;                  # default: null
+        #   presharedKey = null;                       # default: null
+        #   presharedKeyFile = "/run/secrets/wireguard-${config.networking.wireguard.interfaces[0]}";                   # default: null
+        #   publicKey = "";
+        # }
+      ];
+      # allowedIPsAsRoutes = true; # default: true
+      # generatePrivateKeyFile = true; # default: false
+      # ips = [ "192.168.125.1/24" ];
+      # #listenPort = 42270;              # default: null  (51820)
+      # #mtu = 1420;                      # default: null
+      # #interfaceNamespace = null;      # default: null
+      # #postSetup = "";
+      # #postShutdown = "";
+      # #preSetup = "";
+      # #privateKey = "";
+      # #socketNamespace = null; # default: null
+      # #table = "main";         # default: "main"
+    };
   };
 
   networking.firewall = {
-    allowedUDPPorts = [ 51820 42270 42271 42272 42273 42274 42275 ];
+    allowedUDPPorts = let
+      wg-ports = lib.mapAttrsToList (n: v: v.listenPort) config.networking.wireguard.interfaces;
+    in [ 42270 42271 42272 42273 42274 42275 ] ++ wg-ports;
     logReversePathDrops = true; # if packets still dropped, show in dmesg
     #extraCommands = ''
     #  ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
@@ -81,5 +97,8 @@
   #};
 
   # Enable Wireguard network manager service
+  # https://github.com/gin66/wg_netmanager
   services.wg-netmanager.enable = true;
+
+  sops.secrets.wireguard-wg0-privkey = {};
 }
--- nixos/profiles/network/wireguard/wgautomesh.nix
+++ nixos/profiles/network/wireguard/wgautomesh.nix
@@ -1,45 +1,36 @@
 { inputs
-, config
-, lib
-, pkgs
+, config, lib, pkgs
 , ...
 }:
 {
-  inputs = [
-  ];
-
   services.wgautomesh = {
-    enable = true; # Whether to enable the wgautomesh daemon.
+    enable = true;                 # Whether to enable the wgautomesh daemon.
     enableGossipEncryption = true; # Enable encryption of gossip traffic.
-    enablePersistence = true; # Enable persistence of Wireguard peer info between restarts.
-    gossipSecretFile = "/etc/wireguard/wgautomesh-gossip.secret"; # File for gossip secret, shared secret key for gossip encryption. Required if enableGossipEncryption=true. File may contain any arbitrary-length utf8 str. Generate: `openssl rand -base64 32`
+    enablePersistence = true;      # Enable persistence of Wireguard peer info between restarts.
+    gossipSecretFile = config.sops.secrets.wgautomesh-gossip-secret.path; # File for gossip secret, shared secret key for gossip encryption. Required if enableGossipEncryption=true. File may contain any arbitrary-length utf8 str. Generate: `openssl rand -base64 32`
+    openFirewall = true;           # Automatically open gossip port in firewall (recommended).
     logLevel = "info";
-    openFirewall = true; # Automatically open gossip port in firewall (recommended).
 
     settings = {
       gossip_port = 1666; # wgautomesh gossip port, this MUST be the same number on all nodes in the wgautomesh network.
       interface = "wg0"; # Wireguard interface to manage (NOT created by wgautomesh, use another NixOS option to create
       lan_discovery = true; # Enable discovery of peers on the same LAN using UDP broadcast.
+      upnp_forward_external_port = 42270; # Public port to try to redirect to this machine’s Wireguard daemon using UPnP IGD
 
       peers = [
-        {
-          address = "45.42.244.62"; # Wireguard address of this peer (a single IP address, multiple addresses or address ranges are not supported)
+        { address = "45.42.244.62"; # Wireguard address of this peer (a single IP address, multiple addresses or address ranges are not supported)
           endpoint = "45.42.244.130:51420"; # Bootstrap endpoint for connecting to this peer if no address known or none working
           pubkey = "/XgG41yW5zjQvLJl3D6LKVx5UGMb5vmvQc5GHeW/oFc="; # Wireguard public key of this peer.
-          upnp_forward_external_port = 42270; # Public port to try to redirect to this machine’s Wireguard daemon using UPnP IGD
         } # sea1 IPv4
-        {
-          address = "2602:fcc3::c00b";
+        { address = "2602:fcc3::c00b";
           endpoint = "45.42.244.130:51420";
           pubkey = "/XgG41yW5zjQvLJl3D6LKVx5UGMb5vmvQc5GHeW/oFc=";
-          upnp_forward_external_port = 42270;
         } # sea1 IPv6
       ];
-
     };
-
   };
 
   # TODO: Secrets with `agenix` or `sops-nix` (gossipSecretFile)
+  sops.secrets.wgautomesh-gossip-secret = lib.mkIf config.services.wgautomesh.enableGossipEncryption { };
 
 }
