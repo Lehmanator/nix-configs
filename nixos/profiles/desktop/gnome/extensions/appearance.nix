037f1eef397b4b925f105ac822b42af5fe9febfb
Sam Lehman <github@samlehman.dev>
2024-12-09 15:27:52 -0500

    fix: nixosProfiles.desktop.gnome.extensions.appearance - disable incompatible shell extensions rounded-window-corners material-you-color-theming

--- nixos/profiles/desktop/gnome/extensions/appearance.nix
+++ nixos/profiles/desktop/gnome/extensions/appearance.nix
@@ -6,8 +6,8 @@
     compiz-windows-effect #      # Jiggle windows
     gnome-bedtime #              # Smooth transition to grayscale as bedtime approaches
     gradient-top-bar #           # Gradient top bar background
-    material-you-color-theming # # Material You palette from wallpaper applied to adwaita (breaks light/dark theme)
-    rounded-window-corners #     # Round window corners (fix non-adwaita app inconsistency) (11/23: GNOME45 incompat)
+    # material-you-color-theming # # Material You palette from wallpaper applied to adwaita (breaks light/dark theme) # WARN: Not available 24.11
+    # rounded-window-corners #     # Round window corners (fix non-adwaita app inconsistency) # WARN: 11/23: GNOME45 incompat, unavailable 24.11
     snowy #                      # Snow effect on your desktop
   ];
 }
