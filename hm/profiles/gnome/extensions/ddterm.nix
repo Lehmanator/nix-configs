9a7c15b05ce2678e2063825bcfc7d91d01a888ec
Sam Lehman <github@samlehman.dev>
2024-12-09 15:34:55 -0500

    feat: homeProfiles.gnome.extensions.ddterm - use programs.gnome-shell.extensions options, add comments [WIP]

--- hm/profiles/gnome/extensions/ddterm.nix
+++ hm/profiles/gnome/extensions/ddterm.nix
@@ -1,7 +1,16 @@
-{ inputs, ... }:
 { config, lib, pkgs, ... }:
 {
+  # TODO: Fix "Child process killed by signall 11" on launch.
+  #   - Need to add program to PATH?
+  #   - https://github.com/ddterm/gnome-shell-extension-ddterm
   programs.gnome-shell.extensions = [
-    { package = pkgs.gnomeExtensions.ddterm; }
+    { package = pkgs.gnomeExtensions.ddterm;
+      id = "ddterm@amezin.github.com";        # ID needed for auto-enable?
+    }
   ];
+  home.packages = [pkgs.gnomeExtensions.ddterm];
+
+  # TODO: Settings
+  # dconf.settings."com/github/amezin/ddterm" = {
+  # };
 }
