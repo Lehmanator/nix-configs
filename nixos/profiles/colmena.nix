595beb986605488e07e97c7c57d3682272e6340c
Sam Lehman <github@samlehman.dev>
2025-01-02 13:40:50 -0500

    refactor: nixosProfiles.colmena [WIP]

--- nixos/profiles/colmena.nix
+++ nixos/profiles/colmena.nix
@@ -1,43 +1,47 @@
-{ inputs, config, lib, pkgs, user, ... }:
+{ inputs, config, lib, pkgs, ... }:
 {
   # https://github.com/zhaofengli/colmena
-  #imports = with inputs.colmena.nixosModules; [ assertionModule deploymentOptions keyChownModule keyServiceModule metaOptions ];
+  imports = with inputs.colmena.nixosModules; [ 
+    assertionModule 
+    deploymentOptions
+    keyChownModule 
+    keyServiceModule 
+    metaOptions
+  ];
 
   # TODO: Default system config
 
-  #deployment = {
-  #  allowLocalDeployment = lib.mkDefault true;
-  #  buildOnTarget = lib.mkDefault false;
-  #  privilegeEscalationCommand = lib.mkDefault ["sudo" "-H" "--"];
-  #  replaceUnknownProfiles = lib.mkDefault true;
-  #  tags = [];
-  #  targetHost = lib.mkDefault config.networking.hostName;
-  #  targetPort = lib.mkDefault config.services.openssh.port;
-  #  targetUser = lib.mkDefault "root";
-  #  keys = {
-  #  #  name = {
-  #  #    destDir = "/run/keys";
-  #  #    group = "root";
-  #  #    keyCommand = [""];
-  #  #    keyFile = config.sops.secrets.colmena-deployment-key-name.path;
-  #  #    name = "name";
-  #  #    permissions = "0600";
-  #  #    text = "";
-  #  #    uploadAt = "pre-activation"; #"post-activation";
-  #  #    user = "root";
-  #  #};
-  #};
-  #meta = {
-  #  allowApplyAll = lib.mkDefault true;
-  #  description = "A colmena hive";
-  #  machinesFile = "";
-  #  name = config.networking.hostName;
-  #  nixpkgs = inputs.nixpkgs;
-  #  nodeNixpkgs = {};
-  #  nodeSpecialArgs = {inherit inputs pkgs user; };
-  #  specialArgs = {inherit inputs pkgs user; };
-  #};
+  # deployment = {
+  #   allowLocalDeployment = lib.mkDefault true;
+  #   buildOnTarget = lib.mkDefault false;
+  #   privilegeEscalationCommand = lib.mkDefault ["sudo" "-H" "--"];
+  #   replaceUnknownProfiles = lib.mkDefault true;
+  #   tags = [];
+  #   targetHost = lib.mkDefault config.networking.hostName;
+  #   targetPort = lib.mkDefault config.services.openssh.port;
+  #   targetUser = lib.mkDefault "root";
+  #   keys = {
+  #   #  name = {
+  #   #    destDir = "/run/keys";
+  #   #    group = "root";
+  #   #    keyCommand = [""];
+  #   #    keyFile = config.sops.secrets.colmena-deployment-key-name.path;
+  #   #    name = "name";
+  #   #    permissions = "0600";
+  #   #    text = "";
+  #   #    uploadAt = "pre-activation"; #"post-activation";
+  #   #    user = "root";
+  #   #};
+  # };
 
-  # TODO: Learn about `lib.extendModules`
-
+  # meta = {
+  #   allowApplyAll = lib.mkDefault true;
+  #   description = "A colmena hive";
+  #   machinesFile = "";
+  #   name = config.networking.hostName;
+  #   nixpkgs = inputs.nixpkgs;
+  #   nodeNixpkgs = {};
+  #   nodeSpecialArgs = {inherit inputs pkgs user; };
+  #   specialArgs = {inherit inputs pkgs user; };
+  # };
 }
