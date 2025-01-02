cc5ae90f97cb8662e7946320aa4669c9dfeda878
Sam Lehman <github@samlehman.dev>
2025-01-02 17:14:36 -0500

    refactor: homeProfiles.torbrowser - init

--- hm/profiles/apps/browsers.nix
+++ hm/profiles/apps/browsers.nix
@@ -10,9 +10,6 @@
     #./browser-firefox/arkenfox.nix
     #./browser-firefox/librewolf.nix
     #./browser-firefox/iceraven.nix
-
-    #] ++ lib.optionals (pkgs.system == "x86_64-linux") [./browser-tor]
-    #./browser-tor
   ];
 
   # https://gist.github.com/quidome/4e225db4b1611a9624d3927919f96bc6
