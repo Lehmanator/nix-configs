2b2730034560b7ba82591f19a52f42958cb09c43
Sam Lehman <github@samlehman.dev>
2025-01-21 09:09:33 -0500

    refactor: nixosProfiles.flake-registry - remove mkMerge [WIP]

--- nixos/profiles/flake-registry.nix
+++ nixos/profiles/flake-registry.nix
@@ -1,22 +1,22 @@
 { inputs, config, lib, pkgs, ... }:
-let
-  implementation = "divnix";
-in
-lib.mkMerge [
-  (lib.mkIf implementation == "divnix" {
-    imports = [ inputs.quick-nix-registry.nixosModules.local-registry ];
-    nix.localRegistry = {
-      enable = lib.mkDefault true;
-      cacheGlobalRegistry = lib.mkDefault true;
-      noGlobalRegistry = lib.mkDefault false;
-    };
-  }) 
-  (lib.mkIf implementation == "flake-utils-plus" {
-    imports = [ inputs.flake-utils-plus.nixosModules.autoGenFromInputs ];
-    nix = {
-      generateRegistryFromInputs = lib.mkDefault true;
-      generateNixPathFromInputs = lib.mkDefault true;
-      linkInputs = lib.mkDefault true;
-    };
-  })
-]
+# let
+#   implementation = "flake-utils-parts";
+# in
+# if implementation=="flake-utils-parts" then
+{
+  imports = [ inputs.flake-utils-plus.nixosModules.autoGenFromInputs ];
+  nix = {
+    generateRegistryFromInputs = lib.mkDefault true;
+    generateNixPathFromInputs  = lib.mkDefault true;
+    linkInputs = lib.mkDefault true;
+  };
+}
+# else
+# {
+#   imports = [ inputs.quick-nix-registry.nixosModules.local-registry ];
+#   nix.localRegistry = {
+#     enable = lib.mkDefault true;
+#     cacheGlobalRegistry = lib.mkDefault true;
+#     noGlobalRegistry = lib.mkDefault false;
+#   };
+# }
