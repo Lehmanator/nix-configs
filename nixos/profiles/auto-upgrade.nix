fc9e1f5c13148d4ec9aa7f708c19b31405e7d469
Sam Lehman <github@samlehman.dev>
2025-01-21 09:57:47 -0500

    fix: nixosProfiles.auto-upgrade - fix module args repo attrset not using defaults.

--- nixos/profiles/auto-upgrade.nix
+++ nixos/profiles/auto-upgrade.nix
@@ -1,29 +1,30 @@
-{ inputs
-, config
-, lib
-, pkgs
-, repo ? {
+{ inputs, config, lib, pkgs, ... }@moduleArgs:
+let
+  emptyRepo = (
+    builtins.hasAttr "repo" moduleArgs ||
+    !builtins.isAttrs moduleArgs.repo  ||
+    moduleArgs.repo == null            ||
+    moduleArgs.repo == {}
+  );
+  repo = if emptyRepo then {
     protocol = "https";
     host = "github.com";
     user = "Lehmanator";
     org = "Lehmanator";
     name = "nix-configs";
     branch = "main"; #"develop";
-  }
-, ...
-}:
+  } else moduleArgs.repo;
+
+  # Only auto-upgrade if current config came from clean true.
+  isClean = inputs.self ? rev;
+in
 {
-  system.autoUpgrade =
-    let
-      # Only auto-upgrade if current config came from clean true.
-      isClean = inputs.self ? rev;
-    in
-    {
-      enable = isClean;
-      flake = "git+${repo.protocol}://${repo.host}/${repo.org}/${repo.name}.git?ref=${repo.branch}#${config.networking.hostName}";
-      dates = "hourly";
-      flags = [ "--refresh" ];
-    };
+  system.autoUpgrade = {
+    enable = isClean;
+    flake = "git+${repo.protocol}://${repo.host}/${repo.org}/${repo.name}.git?ref=${repo.branch}#${config.networking.hostName}";
+    dates = "hourly";
+    flags = [ "--refresh" ];
+  };
 
   # Only run if current config (self) is older than the new one.
   systemd.services.nixos-upgrade = lib.mkIf config.system.autoUpgrade.enable {
