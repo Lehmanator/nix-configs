ed9ff40960eb26272f988d7407f9d8f2a0ce1f89
Sam Lehman <github@samlehman.dev>
2025-01-21 11:31:38 -0500

    flake: update inputs.lix-module from 2.91.1-2 -> 2.92.0

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
   inputs = {
     # --- Main -----------------------------------------------------
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-2.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # --- Nixpkgs -----------------------------------------------------
