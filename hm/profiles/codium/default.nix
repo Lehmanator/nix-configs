d3ff593e8e33f47a53d5067db0db9e3469dcc272
Sam Lehman <github@samlehman.dev>
2025-01-21 14:52:08 -0500

    feat: homeProfiles.codium - better default config [WIP]

--- hm/profiles/codium/default.nix
+++ hm/profiles/codium/default.nix
@@ -9,4 +9,61 @@
     inputs.nix-vscode-extensions.homeManagerModules.default
   ];
 
+  programs.vscode = {
+    enable = true;
+    enableExtensionUpdateCheck = true;
+    enableUpdateCheck = true;
+    mutableExtensionsDir = true;
+    package = pkgs.vscodium;
+    extensions = [];
+    globalSnippets = {
+      fixme = {
+        prefix = [ "fixme" ];
+        description = "Insert a FIXME remark";
+        body = [ "$LINE_COMMENT FIXME: $0" ];
+      };
+      info = {
+        prefix = [ "info" ];
+        description = "Insert a INFO remark";
+        body = [ "$LINE_COMMENT INFO: $0" ];
+      };
+      note = {
+        prefix = [ "note" ];
+        description = "Insert a NOTE remark";
+        body = [ "$LINE_COMMENT NOTE: $0" ];
+      };
+      todo = {
+        prefix = [ "todo" ];
+        description = "Insert a TODO remark";
+        body = [ "$LINE_COMMENT TODO: $0" ];
+      };
+      warn = {
+        prefix = [ "warn" ];
+        description = "Insert a WARN remark";
+        body = [ "$LINE_COMMENT WARN: $0" ];
+      };
+    };
+    keybindings = [
+      { key = "ctrl+c";
+        command = "editor.action.clipboardCopyAction";
+        when = "textInputFocus";
+        args = { direction = "up"; };
+      }
+    ];
+    # Similar to globalSnippets, but for specific languages.
+    languageSnippets = {};
+    userSettings = {
+      "files.autoSave" = "off";
+      # "[nix]"."editor.tabSize" = 2;
+    };
+    userTasks = {
+      version = "2.0.0";
+      tasks = [
+        # { type = "shell";
+        #   label = "Hello task";
+        #   command = "hello";
+        # }
+      ]
+    }
+  };
 }
