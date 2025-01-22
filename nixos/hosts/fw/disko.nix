0b6693ddcc0841877c86de1d985c08a53955b324
Sam Lehman <github@samlehman.dev>
2025-01-22 11:07:05 -0500

    feat: hosts.fw - iterate on disko configuration [WIP]

--- nixos/hosts/fw/disko.nix
+++ nixos/hosts/fw/disko.nix
@@ -4,6 +4,8 @@
     disk.main = {
       inherit device;
       type = "disk";
+      imageName = "nixos-fw-btrfs-tmpfs";
+      imageSize = "16G";
       partitions = {
         esp = {
           type = "EF00";
@@ -23,7 +25,28 @@
           end = "100%";
           content = {
             type = "luks";
-            name = "nixos";
+            name = "nixos";  # device-mapper name when decrypted
+            extraOpenArgs = {};
+            # NOTE: Secret must be on disk at partition time.
+            #  sops-nix relies on activationScripts to decrypt secrets to /run/secrets
+            #  which will not be activated in the installation image.
+            #  We must find some other mechanism to decrypt the secrets or activate the
+            #  configuration before partitioning.
+            # passwordFile = "/tmp/secret.key";
+            passwordFile = config.sops.secrets.luks-password.path;
+            additionalKeyFiles = [
+              config.sops.secrets.luks-password.path
+              "/tmp/additionalSecret.key"
+            ];
+
+            settings = {
+              # if you want to use the key for interactive login be sure there is no trailing newline
+              # example:  `echo -n "password" > /tmp/secret.key`
+              # keyFile = "/tmp/secret.key";
+              keyFile = config.sops.secrets.luks-password.path;
+              allowDiscards = true;
+            };
+
             content = {
               type = "lvm_pv";
               vg = "nixos";
@@ -36,28 +59,55 @@
       type = "lvm_vg";
       lvs = {
         root = {
-          size = "";
+          size = "100%";
           content = {
             type = "btrfs";
+            extraArgs = ["-f"];  # Override existing partition
             subvolumes = {
-              nix-store.mountpoint = "/nix/store";
-              nix.mountpoint = "/nix";
-              state.mountpoint = "/nix/state";
-              persist.mountpoint = "/nix/persist";
+              home = {
+                mountOptions = ["compress=zstd"];
+                mountpoint = "/home";
+              };
+              # nix-store.mountpoint = "/nix/store";
+              nix = {
+                mountpoint = "/nix";
+                mountOptions = ["compress=zstd" "noatime"];
+              };
+              state = {
+                mountpoint = "/nix/state";
+                mountOptions = ["compress=zstd" "noatime"];
+              };
+              persist = {
+                mountpoint = "/nix/persist";
+                mountOptions = ["compress=zstd" "noatime"];
+              };
+              swap = {
+                mountpoint = "/.swapvol";
+                swap.swapfile.size = "128G";
+              };
             };
           };
         };
-        swap = {
-          name = "swap";
-          size = "100%FREE";
-          content.type = "swap";
-        };
+        # swap = {
+        #   name = "swap";
+        #   size = "100%FREE";
+        #   content.type = "swap";
+        # };
       };
     };
 
-    # TODO: tmpfs for root
+    nodev."/" = {
+      fsType = "tmpfs";
+      mountOptions = ["size=8G" "defaults" "mode=755"];
+    };
   };
 
   fileSystems."/nix/state".neededForBoot = true;
   fileSystems."/nix/persist".neededForBoot = true;
+
+  # https://github.com/nix-community/disko/blob/master/docs/disko-install.md#example-for-a-nixos-installer
+  # https://github.com/nix-community/disko/blob/master/docs/interactive-vm.md
+  virtualisation.vmVariantWithDisko = {
+    virtualisation.fileSystems."/nix/persist".neededForBoot = true;
+  };
 }
