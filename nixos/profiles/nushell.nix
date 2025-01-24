8c6d1df803f255bf6218e918956f9d10db300bee
Sam Lehman <github@samlehman.dev>
2025-01-24 16:08:59 -0500

    fix(nixosProfiles.nushell): remove duplicate environments.pathsToLink

--- nixos/profiles/nushell.nix
+++ nixos/profiles/nushell.nix
@@ -1,14 +1,16 @@
-{ inputs, config, lib, pkgs, ... }: {
+{
+  inputs,
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [
     (inputs.self + /nixos/profiles/shell.nix)
   ];
 
   # TODO: Remove bash, nushell config
   environment = {
-    # Link bash completions. Used by bash & ZSH compat.
-    pathsToLink = ["/share/bash-completion"]
-     ++ lib.optional config.programs.zsh.enable "/share/zsh"
-    ;
     systemPackages = [pkgs.nushell];
     shells = [pkgs.nushell];
   };
