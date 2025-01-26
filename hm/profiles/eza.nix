fec5c63f8668422668281c4c1461dc38bb033970
Sam Lehman <github@samlehman.dev>
2025-01-25 19:44:39 -0500

    refactor(homeProfiles.eza): cleanup & simplify config. Remove unnecessary comments.

--- hm/profiles/eza.nix
+++ hm/profiles/eza.nix
@@ -1,63 +1,39 @@
-{ lib, pkgs, ... }:
-# TODO: Create separate nixosModule / hmModule with these options.
-# - Use option values to set config for all ls programs. `programs.ls = {}`
-# - Use `programs.ls.alias="eza"` to set `programs.eza.enableAliases=true`
-let
-  inherit (lib) optional;
-  sort = {
-    dirs    = "first";
-    field   = "name";
-    reverse = false;
-    ignored = false && show.git;
-  };
-  view = {
-    layout  = "grid"; # grid | tree | oneline
-    recurse = true;
-    depth   = 3;
-  };
-  show = {
-    classify  = false;
-    color     = true;
-    git       = true;
-    icons     = true;
-    headers   = true;
-    links     = true;
-    symlinks  = true;
-    total     = true;
-    hyperlink = true;
-  };
-in {
+{lib, ...}: {
+  # https://github.com/eza-community/eza
+  # https://github.com/eza-community/eza-themes
   programs.eza = {
     enable = true;
     icons = "auto";
-    inherit (show) git;
+    git = true;
 
     # TODO: Use lib.cli.toGNUCommandLine {} attrs;
-    extraOptions =
-      ["--level=${builtins.toString view.depth}"]
-      ++ optional show.hyperlink "--hyperlink"
-      ++ optional show.headers   "--header"
-      ++ optional sort.ignored   "--git-ignore"
-      ++ optional (sort.dirs=="first") "--group-directories-first"
-      #++ lib.optional classify "--classify"
-      #++ lib.optional color "--color-scale=gradient"
-      ;
+    extraOptions = [
+      "--across"
+      # "--classify"
+      "--color=always"
+      "--color-scale=all"
+      "--color-scale-mode=gradiient"
+      # "--git-ignore"
+      "--group-directories-first"
+      "--follow-symlinks"
+      "--hyperlink"
+      "--level=3"
+      "--time-style=relative"
+
+      # --- Long View ---
+      # "--git-repos"
+      "--git-repos-no-status"
+      "--header"
+      "--no-permissions"
+      "--octal-permissions"
+      "--smart-group"
+      "--time=modified"
+      "--total-size"
+    ];
   };
+
+  # TODO: Configure theme here
+  # NOTE: `LS_COLORS` & `EZA_COLORS` take precedence over the theme file
+  # xdg.configFile."eza/theme.yml".text = ''
+  # '';
 }
-#readable = { size = true; date = true; perms = true; };
-# --- Sort Fields ---
-# eza: accessed, changed, created, e|Extension, inode, modified, n|Name, size, type, none (Caps sort upper before lower)
-# lsd:                               extension,            time,   name, size, version
-# --- Time Fields ---
-# eza: modified, changed, accessed, and created
-# lsd:
-# --- Time Styles ---
-# eza: default, iso, long-iso, full-iso
-# lsd:
-# --- Toggle Opts ---
-# exa:  always, never, automatic
-# lsd:  always, never, auto
-# --- Blocks ---
-# exa: permissions, size, user, date, name
-# lsd: permissions, user, group, size, date, name
-# pls:
